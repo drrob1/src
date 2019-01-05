91723d2fa08b4c004c4c4e28fbe8a3421f9464bd
Rob Solomon <git@drrob1.com>
2019-01-05 18:00:01 -0500

    01/05/2019 06:00:05 PM	new file:   fyne-hello/fyne-hello.go

---
+++ fyne-hello/fyne-hello.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	app := app.New()
+
+	quitapp := func() { app.Quit() }
+
+	w := app.NewWindow("Hello")
+	w.SetContent(widget.NewVBox(widget.NewLabel("Hello Fyne"),	widget.NewButton("Quit", quitapp)))
+	w.ShowAndRun()
+}
