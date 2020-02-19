06ca284951aabcfaca12836d2f6c0ff8941f26e8
Rob Solomon <git@drrob1.com>
2020-02-19 07:27:39 -0500

    02/19/2020 07:27:44 AM	qthello2/qthello2.go

--- qthello2/qthello2.go
+++ qthello2/qthello2.go
@@ -6,18 +6,28 @@ import (
 )
 func main() {
    app := widgets.NewQApplication(len(os.Args), os.Args)
+
    window := widgets.NewQMainWindow(nil, 0)
    window.SetWindowTitle("Hello World")
+
    widget := widgets.NewQWidget(window, 0)
    widget.SetLayout(widgets.NewQVBoxLayout())
+
    window.SetCentralWidget(widget)
+
    label := widgets.NewQLabel2("Hello World!", window, 0)
+
    widget.Layout().AddWidget(label)
+
    button := widgets.NewQPushButton2("Quit", window)
-   button.ConnectClicked(func(bool) {
+   onclicked := func(bool) {
       app.QuitDefault()
-   })
+   }
+   button.ConnectClicked(onclicked)
+
    widget.Layout().AddWidget(button)
+
    window.Show()
+   
    widgets.QApplication_Exec()
 }
