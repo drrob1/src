c32aa4dab83f1162ca798974450b30ac8d708770
Rob Solomon <git@drrob1.com>
2020-03-09 19:04:44 -0400

    03/09/2020 basicmenu3.go -- menu works as intended. 	new file:   basicmenu4.go

--- qtexample1/basicmenu4.go
+++ qtexample1/basicmenu4.go
@@ -38,6 +38,7 @@ MainWindow::MainWindow()
 
 Now that basicmenu.go  and basicmenu2.go both work, I'm going to try to define a toolbar.
 for basicmenu3.go, I found out that I don't need the slices stuff for it to work.  It works fine without it.
+I'll remove the menubar and just have the toolbar.
 
 */
 
@@ -55,110 +56,92 @@ func main() {
 	closeIcon := gui.QIcon_FromTheme2("document-close", gui.NewQIcon5("close.png"))
 
 	// set up menu bar, and maybe toolbar
-	menubar := window.MenuBar()
+	//menubar := window.MenuBar()
 
 	// fileactionpointerslice := make([]*widgets.QAction,0,5)  does it work without this?
 
 	// set up file menu option
-	fileMenu := menubar.AddMenu2("&File")
-    a := fileMenu.AddAction2(newIcon,"&New")  // a has type *QAction
+	//fileMenu := menubar.AddMenu2("&File")
+    //a := fileMenu.AddAction2(newIcon,"&New")  // a has type *QAction
     filenewmenuoption := func () {
     	// need a function here.  I'll make it a dummy function
     	widgets.QMessageBox_About(window, "File New", "File New Menu option was selected")
 		return
 	}
-	a.ConnectTriggered(func(checked bool) {
-		filenewmenuoption()
-		return
-	})  // function to execute when option is triggered
+	//a.ConnectTriggered(func(checked bool) { filenewmenuoption() return })  // function to execute when option is triggered
 
-	a.SetPriority(widgets.QAction__LowPriority)
-	a.SetShortcuts2(gui.QKeySequence__New)
+	//a.SetPriority(widgets.QAction__LowPriority)
+	//a.SetShortcuts2(gui.QKeySequence__New)
 
 	//fileactionpointerslice = append(fileactionpointerslice, a)
 
-	b := fileMenu.AddAction2(openIcon, "&Open") // b has type *QAction
+	//b := fileMenu.AddAction2(openIcon, "&Open") // b has type *QAction
     fileopenmenuoption := func () {
     	// need a function here.  I'll make it a dummy function
     	widgets.QMessageBox_About(window, "File Open", "File Open Menu option was selected")
 		return
 	}
-	b.ConnectTriggered(func(checked bool) {
-		fileopenmenuoption()
-		return
-	}) // function to execute when option is triggered
+	//b.ConnectTriggered(func(checked bool) { fileopenmenuoption() return 	}) // function to execute when option is triggered
 
-	b.SetPriority(widgets.QAction__LowPriority)
-	b.SetShortcuts2(gui.QKeySequence__Open)
+	//b.SetPriority(widgets.QAction__LowPriority)
+	//b.SetShortcuts2(gui.QKeySequence__Open)
 
 	//fileactionpointerslice = append(fileactionpointerslice, b)
 
-	fileMenu.AddSeparator()
+	//fileMenu.AddSeparator()
 
-	e := fileMenu.AddAction2(closeIcon, "&Close")
+	//e := fileMenu.AddAction2(closeIcon, "&Close")
 	fileclosemenuoption := func() {
 		widgets.QMessageBox_About(window, "File Close", "File Close menu option was selected")
 		return
 	}
-	e.ConnectTriggered(func(checked bool) {
-		fileclosemenuoption()
-		return
-	})
-	e.SetPriority(widgets.QAction__LowPriority)
-	e.SetShortcuts2(gui.QKeySequence__Close)
+	//e.ConnectTriggered(func(checked bool) { 	fileclosemenuoption() 	return 	})
+	//e.SetPriority(widgets.QAction__LowPriority)
+	//e.SetShortcuts2(gui.QKeySequence__Close)
 	//fileactionpointerslice = append(fileactionpointerslice, e)
 
 	//fileMenu.AddActions(fileactionpointerslice)
 
-	HelpMenu := menubar.AddMenu2("&Help")
+	//HelpMenu := menubar.AddMenu2("&Help")
 	helpIcon := gui.QIcon_FromTheme2("document-help", gui.NewQIcon5("help2.png"))
-	d := HelpMenu.AddAction2(helpIcon, "&Help")
+	//d := HelpMenu.AddAction2(helpIcon, "&Help")
 	filehelpmenuoption := func() {
 		widgets.QMessageBox_About(window, "Help", "Help menu option was selected")
 		return
 	}
-	d.ConnectTriggered(func(checked bool) {
-		filehelpmenuoption()
-		return
-	})
-	d.SetPriority(widgets.QAction__LowPriority)
-	d.SetShortcuts2(gui.QKeySequence__HelpContents)
+	//d.ConnectTriggered(func(checked bool) { 	filehelpmenuoption() 	return 	})
+	//d.SetPriority(widgets.QAction__LowPriority)
+	//d.SetShortcuts2(gui.QKeySequence__HelpContents)
 	//helpactionpointerslice := make([]*widgets.QAction,0,5)
 	//helpactionpointerslice = append(helpactionpointerslice, d)
 	//HelpMenu.AddActions(helpactionpointerslice)
 
 
-	AboutMenu := menubar.AddMenu2("&About")
+	//AboutMenu := menubar.AddMenu2("&About")
 	aboutIcon := gui.QIcon_FromTheme2("document-about", gui.NewQIcon5("about.png"))
-	f := AboutMenu.AddAction2(aboutIcon, "&About")
+	//f := AboutMenu.AddAction2(aboutIcon, "&About")
 	fileaboutmenuoption := func() {
 		widgets.QMessageBox_About(window, "about", "About menu option was selected")
 		return
 	}
-	f.ConnectTriggered(func(checked bool) {
-		fileaboutmenuoption()
-		return
-	})
-	f.SetPriority(widgets.QAction__LowPriority)
-	f.SetShortcuts2(gui.QKeySequence__WhatsThis)
+	//f.ConnectTriggered(func(checked bool) { 	fileaboutmenuoption()	return})
+	//f.SetPriority(widgets.QAction__LowPriority)
+	//f.SetShortcuts2(gui.QKeySequence__WhatsThis)
 	//aboutactionpointerslice := make([]*widgets.QAction,0,5)
 	//aboutactionpointerslice = append(aboutactionpointerslice, f)
 	//AboutMenu.AddActions(aboutactionpointerslice)
 
-	QuitMenu := menubar.AddMenu2("&Quit")
+	//QuitMenu := menubar.AddMenu2("&Quit")
 	quitIcon := gui.QIcon_FromTheme2("document-quit", gui.NewQIcon5("quit-512.png"))
-	c := QuitMenu.AddAction2(quitIcon, "&Quit")
+	//c := QuitMenu.AddAction2(quitIcon, "&Quit")
 	filequitmenuoption := func() {
 		widgets.QMessageBox_About(window, "File Quit", "Quit Menu option was selected")
 		app.Quit()
 		return
 	}
-	c.ConnectTriggered(func(checked bool) {
-		filequitmenuoption()
-		return
-	})
-	c.SetPriority(widgets.QAction__LowPriority)
-	c.SetShortcuts2(gui.QKeySequence__Quit)
+	//c.ConnectTriggered(func(checked bool) {	filequitmenuoption()	return})
+	//c.SetPriority(widgets.QAction__LowPriority)
+	//c.SetShortcuts2(gui.QKeySequence__Quit)
 
 	//quitactionpointerslice := make([]*widgets.QAction,0,5)
 	//quitactionpointerslice = append(quitactionpointerslice, c)
@@ -170,7 +153,6 @@ func main() {
 	// Removing SetToolButtonStyle didn't work.
 	/* */
 	toolbar := widgets.NewQToolBar("tools", window)
-	toolbar.SetAllowedAreas(core.Qt__RightToolBarArea)  // this seems to be ignored.
 	toolbar.SetToolButtonStyle(core.Qt__ToolButtonTextUnderIcon)
 	//toolbar.SetToolButtonStyle(core.Qt__ToolButtonIconOnly)
 	//toolbar.SetToolButtonStyle(core.Qt__ToolButtonFollowStyle)
@@ -185,6 +167,11 @@ func main() {
 	docOpen.ConnectTriggered(func(checked bool) {
 		fileopenmenuoption()
 		return
+	})
+
+	docClose := toolbar.AddAction2(closeIcon, "Close")
+	docClose.ConnectTriggered(func(checked bool) {
+		fileclosemenuoption()
 	})
 
 	Help := toolbar.AddAction2(helpIcon, "Help")
