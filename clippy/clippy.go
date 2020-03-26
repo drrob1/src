7ebb7817cb1301e93de5a402d7e3a8eeebc2afd1
Rob Solomon <git@drrob1.com>
2020-03-26 19:13:02 -0400

    03/26/2020 07:13:07 PM new file:   clippy/clippy.go -- it works, so far

---
+++ clippy/clippy.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+//	"io"
+//	"log"
+
+	//	"io"
+//	"log"
+	"os"
+	"os/exec"
+)
+
+func main() {
+/*
+	buf := []byte(" values written to stdin to go to clipboard ...  ")
+	rdr := bytes.NewReader(buf)
+
+	cmdclip := exec.Command("xclip")
+	cmdclip.Stdin = rdr
+	cmdclip.Stdout = os.Stdout
+	cmdclip.Run()
+
+	fmt.Println()
+	fmt.Println(" written to stdin thru xclip")
+	fmt.Println()
+
+ */
+
+	// Both of these now work on z76.  It just depends on which clip pgm I like better.
+
+	buf := []byte(" string written to stdin to go to xsel ... ")
+	rdr := bytes.NewReader(buf)
+	cmdsel := exec.Command("xsel", "--clipboard", "-i")
+	cmdsel.Stdin = rdr
+	cmdsel.Stdout = os.Stdout
+	cmdsel.Run()
+
+
+	fmt.Println()
+	fmt.Println(" written to stdin thru xsel")
+	fmt.Println()
+
+
+
+}
