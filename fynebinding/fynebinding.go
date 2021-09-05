cc4aa92426049280293e6badc48d8abe480f0502
Rob <git@drrob1.com>
2021-09-05 11:36:20 -0400

    09/05/2021 11:36:23 AM new file:   fynebinding/fynebinding.go

---
+++ fynebinding/fynebinding.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/data/binding"
+	"fyne.io/fyne/v2/widget"
+)
+
+func makeUI() fyne.CanvasObject {
+	f := binding.NewFloat()
+
+	prog := widget.NewProgressBarWithData(f)
+	slide := widget.NewSliderWithData(0, 1, f)
+	slide.Step = 0.01
+	btnfunc := func() { // my edits
+		_ = f.Set(0.5)
+	}
+	btn := widget.NewButton("Set to 0.5", btnfunc) // my edits
+
+	return container.NewVBox(prog, slide, btn)
+}
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Widget Binding")
+
+	w.SetContent(makeUI())
+	w.ShowAndRun()
+}
