ab11e321e5ad33e7a674ab64fff9a472e33fef50
Rob <git@drrob1.com>
2022-01-17 17:05:11 -0500

    01/17/2022 05:05:15 PM On branch master Your branch is ahead of 'origin/master' by 2 commits. mdp2/mdp2.go mdp2/mdp2_test.go

--- mdp2/mdp2.go
+++ mdp2/mdp2.go
@@ -9,6 +9,9 @@ import (
 	"github.com/russross/blackfriday/v2"
 	"io"
 	"os"
+	"os/exec"
+	"runtime"
+	"time"
 )
 
 const lastModified = "17 Jan 2022"
@@ -19,6 +22,7 @@ REVISION HISTORY
 16 Jan 22 -- Started reading chapter 3 in "Powerful Command-Line Applications in Go" by Ricardo Gerardi
 17 Jan 22 -- Adding use of a temporary file for outfile file.
              Now called mdp2.go, and will use interfaces to automate tests, which is the next section in the book
+             Adding an auto-preview feature to this tool.
 */
 
 const header = `<!DOCTYPE html>
@@ -37,6 +41,7 @@ func main() {
 	fmt.Printf("mdp, a Markdown Previewer tool, last modified %s \n", lastModified)
 	var filename string
 	flag.StringVar(&filename, "f", "", "Markdown file to preview")
+	skipPreview := flag.Bool("skip", false, "Skip the preview step.")
 	flag.Parse()
 
 	if filename == "" && flag.NArg() == 0 {
@@ -46,7 +51,7 @@ func main() {
 		filename = flag.Arg(0)
 	}
 
-	err := run(filename, os.Stdout)
+	err := run(filename, os.Stdout, *skipPreview)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
@@ -54,9 +59,10 @@ func main() {
 }
 
 // main() can't be tested using the Go tools, as main() doesn't return anything.  But run() can be, so that's why run() is here as it returns values that can be used in tests.
+// And using the run() function allows the use of a defer statement to clean up our resources.  main() relies on os.Exit() which exits immediately and does not execute any defer statements.
 // Here we will use golden files to validate the output, as the results can be complex as they're entire HTML files.
 // A special subdir off of mdp/ is created, called testdata.  This is ignored by the Go build tools.
-func run(filename string, out io.Writer) error {
+func run(filename string, out io.Writer, skipPreview bool) error {
 	input, err := os.ReadFile(filename)
 	if err != nil {
 		return err
@@ -76,7 +82,16 @@ func run(filename string, out io.Writer) error {
 	outName := temp.Name()
 	fmt.Fprintln(out, outName)
 
-	return saveHTML(outName, htmlData)
+	if err := saveHTML(outName, htmlData); err != nil {
+		return err
+	}
+
+	if skipPreview {
+		return nil
+	}
+
+	defer os.Remove(outName) // only delete this after it is to be previewed, if it's previewed.  But this is a race condition that will be fixed in the preview function.
+	return preview(outName)
 }
 
 func parseContent(input []byte) []byte {
@@ -95,3 +110,50 @@ func parseContent(input []byte) []byte {
 func saveHTML(outFname string, data []byte) error {
 	return os.WriteFile(outFname, data, 0644)
 }
+
+func preview(fname string) error {
+	var cName string
+	var cParams []string
+
+	// define the exec based on OS
+	switch runtime.GOOS {
+	case "linux":
+		cName = "xdg-open"
+	case "windows":
+		cName = "tcc.exe"
+		cParams = []string{"/C", "start"}
+	case "darwin":
+		cName = "open"
+
+	default:
+		return fmt.Errorf("OS not supported")
+	}
+
+	cParams = append(cParams, fname)
+
+	cPath, err := exec.LookPath(cName) // Find the executable file in the PATH
+	if err != nil {
+		return err
+	}
+
+	err = exec.Command(cPath, cParams...).Run()
+
+	// give the browser time to open the file before attempting to delete it.  Better solutions use a signal or a REST api to create a small web server that serves the file directly to the browser.
+	time.Sleep(2 * time.Second)
+	return err
+}
+
+/*
+A bash script to preview a markdown file everytime it changes.  If going to use it, remember to make it executable.
+
+#! /bin/bash
+FHASH=$(md5sum $1)
+while true; do
+  NHASH= $(md5sum $1)
+  if [ "$NHASH" != "$FHASH" ]; then
+    ./mdp $1
+    FHASH=$NHASH
+  fi
+  sleep 5  # means 5 seconds
+done
+*/
