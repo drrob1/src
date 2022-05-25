3edb08b2a6388cf138cf00afff1af8f9476fc8ed
Rob <git@drrob1.com>
2022-05-25 07:40:06 -0400

    05/25/2022 07:40:09 AM feq32/feq32.go -- only edited a comment. 	new file:   feqbbb/feqbbb.go

--- feqbbb/feqbbb.go
+++ feqbbb/feqbbb.go
@@ -1,11 +1,9 @@
-package main // for feq32.go
+package main // for feqbbb.go
 
 import (
 	"bufio"
 	"flag"
 	"fmt"
-	"hash"
-	"hash/crc32"
 	"io"
 	"os"
 	"runtime"
@@ -54,17 +52,12 @@ import (
                Now called feq64.go, and will only compute crc64 ECMA checksum, in a way that only opens 1 file at a time.  And it doesn't need a bytes.Reader.
   21 May 22 -- Since I want to be able to process huge files > 3 GB, I can't read in the entire file at once.  I'll switch to a file reader algorithm.
                   Now called feq32, to play w/ the crc32 functions.  Since I intend this for huge files (> 3 GB), I'll use a flag to determine which crc32 to use.
+  24 May 22 -- Now called feqbbb, for byte by byte.  I'll read in chunks and compare them, so very large files can be handled, too.
 */
 
-const LastCompiled = "21 May 2022"
-
-const (
-	ieeePoly = iota
-	castPoly
-	koopPoly
-)
-
-var polyNames = []string{"IEEE", "Castagnoli", "Koopman"} // this is a slice of strings, not an array of strings.  But this is a fine point that doesn't really matter.
+const LastCompiled = "25 May 2022"
+const K = 1024
+const M = K * K
 
 //* ************************* MAIN ***************************************************************
 func main() {
@@ -78,23 +71,19 @@ func main() {
 
 	// flag help message
 	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), " file equal tester only using 1 crc32 selected by a switch, last modified %s, compiled with %s.\n", LastCompiled, runtime.Version())
+		fmt.Fprintf(flag.CommandLine.Output(), " file equal byte by byte tester, last modified %s, compiled with %s.\n", LastCompiled, runtime.Version())
 		fmt.Fprintf(flag.CommandLine.Output(), " Filenames to compare are given on the command line.\n")
 		fmt.Fprintf(flag.CommandLine.Output(), " %s has timestamp of %s.  Working directory is %s.  Full name of executable is %s.\n",
 			ExecFI.Name(), LastLinkedTimeStamp, workingDir, execName)
 		fmt.Fprintln(flag.CommandLine.Output())
 		flag.PrintDefaults()
 	}
-	var verboseFlag, ieeeFlag, castFlag, koopFlag bool
-	var polynomial int // used to determine which one of the three to use.  Default will be IEEE, as it's the fastest and most widely used because of this.
+	var verboseFlag bool
 	flag.BoolVar(&verboseFlag, "v", false, " verbose mode.")
-	flag.BoolVar(&ieeeFlag, "ie", false, " IEEE polynomial flag.")
-	flag.BoolVar(&castFlag, "cast", false, " Castagnoli polynomial flag.")
-	flag.BoolVar(&koopFlag, "koop", false, "Koopman polynomial flag.")
 	flag.Parse()
 
 	if verboseFlag {
-		fmt.Printf("\n feq32 File equal using 1 of 3 crc32 polynomials, last modified %s, compiled by %s\n\n", LastCompiled, runtime.Version())
+		fmt.Printf("\n feqbbb File equal using byte by byte comparisons of chunks, last modified %s, compiled by %s\n\n", LastCompiled, runtime.Version())
 	}
 
 	if flag.NArg() == 0 {
@@ -108,77 +97,86 @@ func main() {
 		os.Exit(1)
 	}
 
-	if ieeeFlag { // notice that this will be default if no polynomial flags are used.
-		polynomial = ieeePoly
-	} else if castFlag {
-		polynomial = castPoly
-	} else if koopFlag {
-		polynomial = koopPoly
+	openedFile1, err1 := os.Open(filename1)
+	check(err1, " Reading first file error is")
+	defer openedFile1.Close()
+	fi1, e1 := openedFile1.Stat()
+	if e1 != nil {
+		fmt.Fprintf(os.Stderr, " Opening %s and error is: %v.  Exiting. \n", filename1, e1)
+		os.Exit(1)
 	}
+	fileReader1 := bufio.NewReader(openedFile1)
 
-	//fileByteSlice, err := os.ReadFile(filename1)  Needs too much memory if file is huge, like when > 3 or 4 GB.
-	openedFile, err := os.Open(filename1)
-	check(err, " Reading first file error is")
-	fileReader := bufio.NewReader(openedFile)
+	openedFile2, err2 := os.Open(filename2)
+	check(err2, " Reading 2nd file error is")
+	defer openedFile2.Close()
+	fi2, e2 := openedFile2.Stat()
+	if e2 != nil {
+		fmt.Fprintf(os.Stderr, " Opening %s and error is %v.  Exiting.\n", filename2, e2)
+		os.Exit(1)
+	}
+	fileReader2 := bufio.NewReader(openedFile2)
 
-	// compute the hash first for file 1, then for file 2, then compare them and output results.
-
-	var crc32Table *crc32.Table
-	var crc32Hash1, crc32Hash2 hash.Hash32
-
-	// first file's first.
-	if polynomial == ieeePoly {
-		crc32Hash1 = crc32.NewIEEE()
-		crc32Hash2 = crc32.NewIEEE()
-	} else if polynomial == castPoly {
-		crc32Table = crc32.MakeTable(crc32.Castagnoli)
-		crc32Hash1 = crc32.New(crc32Table)
-		crc32Hash2 = crc32.New(crc32Table)
-	} else if polynomial == koopPoly {
-		crc32Table = crc32.MakeTable(crc32.Koopman)
-		crc32Hash1 = crc32.New(crc32Table)
-		crc32Hash2 = crc32.New(crc32Table)
-	} else {
-		fmt.Fprintf(os.Stderr, " polynomial variable value not valid.  It is %d.  Exiting\n", polynomial)
+	if fi1.Size() != fi2.Size() {
+		fmt.Printf(" Files not equal as their sizes are not equal.  %s size is %d, and %s size is %d.\n", filename1, fi1.Size(), filename2, fi2.Size())
 		os.Exit(1)
 	}
 
+	buf1 := make([]byte, M) // I initially wrote this as ([]byte,0,M) so the slice was 0 bytes long.  This is what I need when using append() as in other code.
+	buf2 := make([]byte, M) // But it's completely wrong here.  The backing array of size M is irrelevant; the slice behaves as a buffer of length 0.
+
 	t0 := time.Now()
-	io.Copy(crc32Hash1, fileReader)
-	crc32Val1 := crc32Hash1.Sum32()
+	matched := true
 
-	if verboseFlag {
-		fmt.Printf(" file 1 %s: %s polynomial, hash1 = %x, elapsed time = %s\n\n", filename1, polyNames[polynomial], crc32Val1, time.Since(t0))
-	}
+	var counter int
 
-	// second file's second, and then comparing the values.
-	openedFile, err = os.Open(filename2)
-	check(err, " Reading 2nd file error is")
-	fileReader = bufio.NewReader(openedFile)
-
-	// crc32 section for file 2
-	io.Copy(crc32Hash2, fileReader)
-	crc32Val2 := crc32Hash2.Sum32()
-
-	if crc32Val1 == crc32Val2 {
-		if verboseFlag {
-			fmt.Printf(" crc32 %s polynomial values for %s and %s are equal.  Total elapsed time is %s.\n\n", polyNames[polynomial], filename1, filename2, time.Since(t0))
-		} else {
-			fmt.Printf(" crc32 %s polynomial values are equal.  Total elapsed time is %s.\n", polyNames[polynomial], time.Since(t0))
+outerLoop:
+	for { //outer loop to refill the buffers
+		n1, er1 := fileReader1.Read(buf1)
+		n2, er2 := fileReader2.Read(buf2)
+		counter++
+		if er1 != nil || er2 != nil {
+			if er1 == io.EOF || er2 == io.EOF {
+				break outerLoop
+			}
+			fmt.Fprintf(os.Stderr, " File read errors.  %s err is %v, %s err is %v\n\n", filename1, er1, filename2, er2)
+			break outerLoop
 		}
-	} else {
-		fmt.Printf(" crc32 %s polynomial for the files are not equal.  %s = %x, %s = %x.  Total elapsed time is %s.\n\n",
-			polyNames[polynomial], filename1, crc32Val1, filename2, crc32Val2, time.Since(t0))
+		if n1 != n2 { // should never happen.  If file sizes are different should be caught above by the stat calls.
+			matched = false
+			fmt.Printf(" n1 is %d, n2 is %d\n", n1, n2)
+			break outerLoop
+		}
+		if n1 == 0 || n2 == 0 { // I don't know if this will ever happen.
+			break outerLoop
+		}
+
+		for i := range buf1 {
+			if buf1[i] != buf2[i] {
+				matched = false
+				fmt.Printf(" First mismatching character is at position %d in loop %d.\n", i, counter)
+				if verboseFlag {
+					fmt.Printf(" The mismatched characters are %c in %s, and %c in %s.\n", buf1[i], filename1, buf2[i], filename2)
+				}
+				break outerLoop
+			}
+		}
 	}
 
 	if verboseFlag {
-		fmt.Printf(" file 2 %s: crc32 %s polynomial = %x, total elapsed time = %s. \n\n",
-			polyNames[polynomial], filename2, crc32Val2, time.Since(t0))
+		fmt.Printf(" Outer loop counter is %d.  %s and %s are ", counter, filename1, filename2)
+	} else {
+		fmt.Print(" Files are ")
 	}
 
-	fmt.Println()
-	fmt.Println()
-} // Main for feq32.go.
+	if matched {
+		fmt.Print("equal")
+	} else {
+		fmt.Print("NOT equal")
+	}
+	fmt.Printf(".  Elapsed time is %s\n\n", time.Since(t0))
+
+} // Main for feqbbb.go.
 
 // ------------------------------------------------------- check -------------------------------
 func check(e error, msg string) {
