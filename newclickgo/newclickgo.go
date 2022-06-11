90e4be8f727b1a8b3b450b4f9e60f2fd7fe220f0
Rob Solomon <github@drrob1.com>
2022-06-10 21:52:40 -0400

    06/10/2022 21:52:47 newclickgo/newclickgo.go -- pets not quite working.  Processes part is working

--- newclickgo/newclickgo.go
+++ newclickgo/newclickgo.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	fg "github.com/audrenbdb/goforeground"
+	"github.com/go-vgo/robotgo"
 	ps "github.com/mitchellh/go-ps"
 	"os"
 	"runtime"
@@ -16,14 +18,21 @@ import (
   HISTORY
   -------
   8 Jun 22 -- Started playing w/ this.  This will take a while, as I have SIR in Boston soon.
+  10 June 22 -- Seems to be mostly working.  Tomorrow going to Boston.
 
 */
 
-const lastModified = "June 9, 2022"
+const lastModified = "June 10, 2022"
 
 var verboseFlag bool
 var pid int
 var target string
+
+type pet struct {
+	pid   int32
+	exec  string
+	title string
+}
 
 func main() {
 	fmt.Printf("newclickgo is my attempt to use Go to activate a process so I can click on the screen.  Last modified %s.  Compiled by %s \n",
@@ -66,4 +75,63 @@ func main() {
 
 	fmt.Printf(" There are %d processes found by go-ps.\n", len(processes))
 
+	pause()
+
+	pets := make([]pet, 0, len(processes))
+	for i := range processes {
+		piD := int32(processes[i].Pid())
+		apet := pet{ // meaning a pet
+			pid:   piD,
+			exec:  strings.ToLower(processes[i].Executable()),
+			title: robotgo.GetTitle(piD),
+		}
+		pets = append(pets, apet)
+	}
+
+	fmt.Println(robotgo.GetTitle())
+	ids, er := robotgo.FindIds("")
+	if er != nil {
+		fmt.Printf(" Error from robotgo FindIds is %v.  Exiting\n")
+		os.Exit(1)
+	}
+	name, _ := robotgo.FindName(ids[100])
+	fmt.Printf(" robotgo GetTitle for id[%d], title is %q, and name is %q\n", ids[100], robotgo.GetTitle(ids[100]), name)
+
+	fmt.Printf(" Will now show you my pets.\n")
+	pause()
+
+	for _, peT := range pets {
+		fmt.Printf(" PID=%d, exe=%q, Title=%q\n", peT.pid, peT.exec, peT.title)
+	}
+	fmt.Printf(" There are %d pets and %d processes.\n", len(pets), len(processes))
+
+	pause()
+
+	var piD int32
+	for _, peT := range pets {
+		if target != "" && (strings.Contains(peT.title, target) || strings.Contains(peT.exec, target)) {
+			piD = peT.pid
+			if !verboseFlag { // if verbose, show all processes even after find a match w/ target.
+				break
+			}
+		}
+	}
+
+	if piD != 0 { // piD == 0 when target is not found.  Don't want to activate process 0.
+		err2 := fg.Activate(int(piD))
+		if err2 != nil {
+			fmt.Printf(" Error from fg.Activate is %v.  Exiting \n", err2)
+			os.Exit(1)
+		}
+	}
+
+}
+
+// --------------------------------------------------------------------------------------------
+
+func pause() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print(" Pausing.  Hit <enter> to continue  ")
+	scanner.Scan()
+	_ = scanner.Text()
 }
