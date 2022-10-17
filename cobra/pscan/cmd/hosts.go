4db7374c9efc59370300558b9c924b703aa5619e
Rob <git@drrob1.com>
2022-10-16 22:32:21 -0400

    10/16/2022 10:32:25 PM cobra/pscan/LICENSE cobra/pscan/cmd/hosts.go cobra/pscan/cmd/root.go cobra/pscan/main.go

---
+++ cobra/pscan/cmd/hosts.go
@@ -0,0 +1,53 @@
+/*
+Copyright © 2022 Robert Solomon
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// hostsCmd represents the hosts command
+var hostsCmd = &cobra.Command{
+	Use:   "hosts",
+	Short: "Manage the hosts list",
+	Long: `Manages the hosts lists for pscan
+
+Add hosts w/ the add command
+Delete hosts w/ the delete command
+List hosts w/ the list command.`,
+
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("hosts called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(hostsCmd) // attach the hostsCmd command to the root.
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// hostsCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// hostsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
---
+++ cobra/pscan/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 Robert Solomon <rob@drrob1.com>
+
+*/
+package main
+
+import "src/cobra/pscan/cmd"
+
+func main() {
+	cmd.Execute()
+}
