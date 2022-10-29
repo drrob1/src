cbdd026fef12746e0a81b78b16f11398ace379ba
Rob <git@drrob1.com>
2022-10-28 22:30:33 -0400

    10/28/2022 10:30:36 PM since/since.go -- added deviceID stuff

--- since/since.go
+++ since/since.go
@@ -5,26 +5,33 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/MichaelTJones/walk"
+	jwalk "github.com/MichaelTJones/walk"
 	"log"
 	"os"
+	"path/filepath"
 	"runtime"
 	"sort"
 	"strings"
 	"sync"
+	"syscall"
 	"time"
 )
 
-/* REVISION HISTORY
+/*
+   REVISION HISTORY
+   -------- -------
    21 Oct 2018 -- First started playing w/ MichaelTJones' code.  I added a help flag
    21 Oct 2022 -- In the code again, after running golangci-lint.  Changed how help flag contents is output.  If an absolute time is given, use that, else use duration.
    26 Oct 2022 -- Added '~' processing.  And output timing info.
+   28 Oct 2022 -- On linux this takes an hour to run when I invoke it using my home directory.  I'm looking into why.  I think because it's following a symlink to bigbkupG.
+                  Naw, it's also following symlinks to DSM.
+                  I posted on golang-nuts for help.  I'm adding the DevID that was recommended.
 */
 
-var LastAlteredDate = "Oct 26, 2022"
+var LastAlteredDate = "Oct 28, 2022"
 
 //var duration = flag.String("d", "", "find files modified within DURATION")
-var duration = flag.Duration("dur", 5*time.Minute, "find files modified within this duration")
+var duration = flag.Duration("dur", 10*time.Minute, "find files modified within this duration")
 var format = flag.String("f", "2006-01-02 03:04:05", "time format")
 var instant = flag.String("t", "", "find files modified since TIME")
 var quiet = flag.Bool("q", false, "do not print filenames")
@@ -32,9 +39,11 @@ var verbose = flag.Bool("v", false, "print summary statistics")
 var days = flag.Int("d", 0, "days duration")
 var weeks = flag.Int("w", 0, "weeks duration")
 
-//var help = flag.Bool("h", false, "print help message")
+type devID uint64
 
 func main() {
+
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	execName, _ := os.Executable()
 	ExecFI, _ := os.Stat(execName)
@@ -98,12 +107,57 @@ func main() {
 	var lock sync.Mutex
 	var tFiles, tBytes int // total files and bytes
 	var rFiles, rBytes int // recent files and bytes
+	var rootDeviceID devID
+	var dir string
+
+	if len(flag.Args()) < 1 {
+		dir, err = os.Getwd()
+		if err != nil {
+			log.Fatalln(" error from Getwd is", err)
+		}
+	} else {
+		dir = flag.Arg(0) // will only use the first argument, which is all I use anyway.
+		dir = strings.Replace(dir, "~", home, 1)
+	}
+	fi, er := os.Stat(dir)
+	if er != nil {
+		log.Fatalf(" error from os.Stat(%s) is %v\n", dir, er)
+	}
+	rootDeviceID = GetDeviceID(dir, fi)
+
 	sizeVisitor := func(path string, info os.FileInfo, err error) error {
 		if err == nil {
 			lock.Lock()
 			tFiles += 1
 			tBytes += int(info.Size())
 			lock.Unlock()
+
+			if info.IsDir() {
+				if filepath.Ext(path) == ".git" {
+					if *verbose {
+						fmt.Printf(" skipping .git\n")
+					}
+					return filepath.SkipDir
+					//} else if strings.Contains(path, ".cache") {
+					//	if *verbose {
+					//		fmt.Printf(" skipping .cache\n")
+					//	}
+					//	return filepath.SkipDir
+				} else if isSymlink(info.Mode()) { // skip all symlinked directories.  I intend this to catch bigbkupG and DSM.
+					if *verbose {
+						fmt.Printf(" skipping symlink %s\n", path)
+					}
+					return filepath.SkipDir
+				} else {
+					id := GetDeviceID(path, info)
+					if rootDeviceID != id {
+						if *verbose {
+							fmt.Printf(" root device id is %d for %q, path device id is %d for %q.  Skipping.\n", rootDeviceID, dir, id, path)
+						}
+						return filepath.SkipDir
+					}
+				}
+			}
 
 			if info.ModTime().After(when) {
 				lock.Lock()
@@ -119,28 +173,21 @@ func main() {
 		}
 		return nil
 	}
-	if len(flag.Args()) < 1 {
-		dir, err := os.Getwd()
-		if err != nil {
-			log.Fatalln(" error from Getwd is", err)
-		}
-		err = walk.Walk(dir, sizeVisitor)
-		if err != nil {
-			log.Fatalln(" error from walk.Walk is", err)
-		}
+
+	if *quiet { // just so compiler sees this can potentially still be executed.
+		err = jwalk.Walk(dir, sizeVisitor)
 	} else {
-		for _, root := range flag.Args() {
-			dir := strings.Replace(root, "~", home, 1) // I decided to not test for windows or presence of ~.  This is pretty fast as it is.
-			err := walk.Walk(dir, sizeVisitor)
-			if err != nil {
-				log.Fatalln(" error from walk.Walk is", err)
-			}
-		}
+		err = filepath.Walk(dir, sizeVisitor)
+	}
+
+	if err != nil {
+		log.Printf(" error from walk.Walk is %v\n", err)
 	}
 
 	// wait for traversal results and print
-	close(results) // no more results
-	<-done         // wait for final results and sorting
+	time.Sleep(3 * time.Second) // kludge to see if I really do need a waitgroup.  I think I do after all.
+	close(results)              // no more results
+	<-done                      // wait for final results and sorting
 	𝛥t := float64(time.Since(now)) / 1e9
 
 	for _, r := range result {
@@ -159,4 +206,14 @@ func main() {
 		log.Printf("    recent: %8d files (%7.2f%%), %13d bytes (%7.2f%%) in %.4f seconds\n",
 			rFiles, rfp, rBytes, rbp, 𝛥t)
 	}
+}
+
+func isSymlink(fm os.FileMode) bool {
+	intermed := fm & os.ModeSymlink
+	return intermed != 0
+}
+
+func GetDeviceID(path string, fi os.FileInfo) devID {
+	var stat = fi.Sys().(*syscall.Stat_t)
+	return devID(stat.Dev)
 }
