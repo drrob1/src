616e06347230756a7a445349ddc7a915b310fc9f
Rob <git@drrob1.com>
2022-10-30 20:14:38 -0400

    10/30/2022 08:14:41 PM since/since.go

--- since/since.go
+++ since/since.go
@@ -31,7 +31,7 @@ import (
                   Turns out that the syscall used by GetDeviceID won't compile on Windows, so I have to use platform specific code for it.  I'll do that now.
 */
 
-var LastAlteredDate = "Oct 29, 2022"
+var LastAlteredDate = "Oct 30, 2022"
 
 //var duration = flag.String("d", "", "find files modified within DURATION")
 var duration = flag.Duration("dur", 10*time.Minute, "find files modified within this duration")
