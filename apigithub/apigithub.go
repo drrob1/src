fe64de05abbc38d299b93ef6303e7308c4595b2d
Rob Solomon <github@drrob1.com>
2022-11-30 17:12:11 -0500

    11/30/2022 17:12:15# Changes to be committed: apigithub/apigithub.go

--- apigithub/apigithub.go
+++ apigithub/apigithub.go
@@ -1,11 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
@@ -17,14 +16,27 @@ func main() {
 	if resp.StatusCode != http.StatusOK {
 		log.Fatalf(" Github api err returned is: %s\n", err)
 	}
-	respBody, er := io.ReadAll(resp.Body)
-	if er != nil {
-		log.Fatalf(" io.ReadAll(resp.Body) err returned is: %s\n", er)
-	}
-	fmt.Printf(" respnse header content-type is: %#v\n", resp.Header.Get("Content-Type"))
+	//respBody, er := io.ReadAll(resp.Body)
+	//if er != nil {
+	//	log.Fatalf(" io.ReadAll(resp.Body) err returned is: %s\n", er)
+	//}
+	//fmt.Printf(" respnse header content-type is: %#v\n", resp.Header.Get("Content-Type"))
 
-	err = os.WriteFile("api_github_com_drrob1.txt", respBody, 0666)
-	if err != nil {
-		fmt.Printf(" Error from os.WriteFile(api-github) is: %s\n", err)
+	//err = os.WriteFile("api_github_com_drrob1.txt", respBody, 0666)
+	//if err != nil {
+	//	fmt.Printf(" Error from os.WriteFile(api-github) is: %s\n", err)
+	//}
+
+	type Reply struct {
+		Name string
+		//Public_Repos int  this worked, but I want to do it now w/ field tags, just to demonstrate this.
+		NumOfRepos int `json:"public_repos"`
 	}
+	var r Reply
+
+	dec := json.NewDecoder(resp.Body)
+	if err := dec.Decode(&r); err != nil { // since 'r' has to be changed by the decoder, it must take a pointer instead of a value.
+		log.Fatalf(" can't decode, error is %s\n", err)
+	}
+	fmt.Printf(" %#v\n", r)
 }
