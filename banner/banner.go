94ad52299f44a1d31d329c275931215120fea59e
Rob <git@drrob1.com>
2022-11-29 19:39:32 -0500

    11/29/2022 07:39:34 PM banner/banner.go

--- banner/banner.go
+++ banner/banner.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/dimiro1/banner"
 	"github.com/mattn/go-colorable"
+	"strconv"
 )
 
 /*
@@ -14,9 +15,11 @@ import (
 func main() {
 	bannerIsEnabled := true
 	bannerIsColorEnabled := true
-	numStr := "23.45"
+	//numStr := "23.45"
+	numStr := strconv.FormatFloat(3.14159, 'g', -1, 64)
 	numStr2 := "-12.98"
 	text := "{{" + ".Title \"" + numStr + "\"  \"banner\" 0" + "}}" + numStr2
+	text2 := "{{.AnsiColor.Yellow}}" + text + "{{.AnsiColor.Default}}"
 	banner.InitString(colorable.NewColorableStdout(), bannerIsEnabled, bannerIsColorEnabled, "{{ .Title  \"My Title 1\"  \"banner\" 0  }}")
 	fmt.Println()
 	banner.InitString(colorable.NewColorableStdout(), bannerIsEnabled, bannerIsColorEnabled, "v={{.GoVersion}}  compiler={{.Compiler}} numCPU={{.NumCPU}}")
@@ -29,6 +32,6 @@ func main() {
 	//fmt.Println()
 	banner.InitString(colorable.NewColorableStdout(), bannerIsEnabled, bannerIsColorEnabled, " some text first:\n{{ .Title  \"My Title\"  \"banner\" 0  }}")
 	fmt.Println()
-	banner.InitString(colorable.NewColorableStdout(), bannerIsEnabled, bannerIsColorEnabled, text)
+	banner.InitString(colorable.NewColorableStdout(), bannerIsEnabled, bannerIsColorEnabled, text2)
 	fmt.Println()
 }
