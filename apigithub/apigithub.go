999a5e522baea366fd219e20142102d1cf5809ae
Rob Solomon <github@drrob1.com>
2022-11-30 17:48:15 -0500

    11/30/2022 17:48:18 apigithub/apigithub.go

--- apigithub/apigithub.go
+++ apigithub/apigithub.go
@@ -5,17 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 )
 
+//type Reply struct {  moved below to be an anonymous struct
+//	Name string
+//	//Public_Repos int  this worked, but I want to do it now w/ field tags, just to demonstrate this.
+//	NumOfRepos int `json:"public_repos"`
+//}
+
 func main() {
-	resp, err := http.Get("https://api.github.com/users/drrob1")
-	if err != nil {
-		log.Fatalf(" Github api err returned is: %s\n", err)
-	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		log.Fatalf(" Github api err returned is: %s\n", err)
-	}
 	//respBody, er := io.ReadAll(resp.Body)
 	//if er != nil {
 	//	log.Fatalf(" io.ReadAll(resp.Body) err returned is: %s\n", er)
@@ -27,16 +26,37 @@ func main() {
 	//	fmt.Printf(" Error from os.WriteFile(api-github) is: %s\n", err)
 	//}
 
-	type Reply struct {
+	//fmt.Printf(" %#v\n", r)
+
+	name, publicRepos, err := githubInfo("drrob1")
+	if err != nil {
+		log.Fatalf(" err from githubInfo is: %s\n", err)
+	}
+	fmt.Printf(" github name: %s, number of public repos = %d\n", name, publicRepos)
+}
+
+// Will take code from main (above) and place it into this func.  This is a user exercise.
+func githubInfo(login string) (string, int, error) {
+	urL := "https://api.github.com/users/" + url.PathEscape(login) // this is to make sure that the login string will result in a valid url.
+	resp, err := http.Get(urL)
+	if err != nil {
+		log.Printf(" Github api err returned is: %s\n", err)
+		return "", 0, err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		log.Printf(" Github api err returned is: %s\n", err)
+		return "", 0, err
+	}
+
+	var r struct { // now it's an anonymous struct, which is fine since it's not needed anywhere else in this code.
 		Name string
 		//Public_Repos int  this worked, but I want to do it now w/ field tags, just to demonstrate this.
 		NumOfRepos int `json:"public_repos"`
 	}
-	var r Reply
-
-	dec := json.NewDecoder(resp.Body)
-	if err := dec.Decode(&r); err != nil { // since 'r' has to be changed by the decoder, it must take a pointer instead of a value.
+	decod := json.NewDecoder(resp.Body)      // Remember that unused fields in either the json or the Go struct will be ignored without being an error.
+	if err := decod.Decode(&r); err != nil { // since 'r' has to be changed by the decoder, it must take a pointer instead of a value.
 		log.Fatalf(" can't decode, error is %s\n", err)
 	}
-	fmt.Printf(" %#v\n", r)
+	return r.Name, r.NumOfRepos, nil
 }
