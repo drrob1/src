61d0ed108a082c768ecdde35571831e7acdb063d
Rob Solomon <github@drrob1.com>
2023-02-11 08:49:43 -0500

    02/11/2023 08:49:47 hello/hello.go -- test Go version detection code.

--- hello/hello.go
+++ hello/hello.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"runtime"
 	"strconv"
 )
 import format "fmt"
@@ -11,23 +11,35 @@ func main() {
 	fmt.Printf("Hello World. \n")
 	fmt.Println("Hello World line 2.")
 	format.Println("Called as format.Println")
-	if n, err := fmt.Printf("Hello World. \n"); err != nil {
-		os.Exit(1)
-	} else {
-		fmt.Printf(" Printed %d characters.\n", n)
-	}
-	s := "20.1"
-	n, err := strconv.Atoi(s)
+	//if n, err := fmt.Printf("Hello World. \n"); err != nil {
+	//	os.Exit(1)
+	//} else {
+	//	fmt.Printf(" Printed %d characters.\n", n)
+	//}
+	//s := "20.1"
+	//n, err := strconv.Atoi(s)
+	//if err != nil {
+	//	fmt.Printf(" Conversion of 20.1 returned error of %s\n", err)
+	//} else {
+	//	fmt.Printf(" Conversion of 20.1 returned no error and a result of %d\n", n)
+	//}
+	//s = "20."
+	//n, err = strconv.Atoi(s)
+	//if err != nil {
+	//	fmt.Printf(" Conversion of 20. returned error of %s\n", err)
+	//} else {
+	//	fmt.Printf(" Conversion of 20. returned no error and a result of %d\n", n)
+	//}
+	goVersion := runtime.Version()
+	goVersionS1 := goVersion[4:6] // this should be a string of characters 4 and 5, or the numerical digits after Go1.  At the time of writing this, it will be 20.
+	goVersionS2 := goVersion[4:]  // this may have a "." and a revision number.
+	goVersionInt, err := strconv.Atoi(goVersionS1)
 	if err != nil {
-		fmt.Printf(" Conversion of 20.1 returned error of %s\n", err)
-	} else {
-		fmt.Printf(" Conversion of 20.1 returned no error and a result of %d\n", n)
+		fmt.Printf(" Go version Atoi Err: %s\n", err)
 	}
-	s = "20."
-	n, err = strconv.Atoi(s)
+	goVersionFloat, err := strconv.ParseFloat(goVersionS2, 64)
 	if err != nil {
-		fmt.Printf(" Conversion of 20. returned error of %s\n", err)
-	} else {
-		fmt.Printf(" Conversion of 20. returned no error and a result of %d\n", n)
+		fmt.Printf(" Go version ParseFloat Err: %s\n", err)
 	}
+	fmt.Printf(" Go version int = %d, float = %f\n", goVersionInt, goVersionFloat)
 }
