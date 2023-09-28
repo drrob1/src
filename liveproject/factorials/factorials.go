fc07722202424dff42ede90b1452d02cae6d6b7c
Rob Solomon <github@drrob1.com>
2023-09-28 07:40:40 -0400

    On branch master 09/28/2023 07:40:46 liveproject/factorials/factorials.go

--- liveproject/factorials/factorials.go
+++ liveproject/factorials/factorials.go
@@ -1,7 +1,23 @@
 package main
 
+import "fmt"
+
 // factorials.go for the factorials project part of the recursion liveproject
 
-func main() {
+func factorial(n int64) int64 {
+	if n < 0 {
+		return 0
+	} else if n < 2 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
 
+func main() {
+	var n int64
+
+	for n = 0; n <= 21; n++ {
+		fmt.Printf(" %03d! = %20d\n", n, factorial(n))
+	}
+	fmt.Println()
 }
