1226136f2f03d7dde468701afef7d7cb134e3624
Rob Solomon <github@drrob1.com>
2023-10-02 19:17:58 -0400

    On branch master 10/02/2023 19:18:02 liveproject/knight/knight.go

--- liveproject/knight/knight.go
+++ liveproject/knight/knight.go
@@ -13,6 +13,8 @@ import (
 // I just looked at the partial solution.  The author says that for an 8 x 8 board, the solution takes ~11.6 minutes if open, and > 15 min if closed.
 // I need a longer timeout.  I'll try 15 min and see how it goes.
 // Nope, that didn't work.  The timeout triggered before a solution did.  I'll make it 30 min and see what happens.
+// I changed the initialize Offsets() to be as used by Stephens, and suddenly the 6 x 6 took ~ 1/2 sec, and the 8 x 8 took ~8 sec.  Down from > 30 min even for the 6 x 6.  Wow.
+// Turns out that the order of the different moves in the Offset struct matters. A lot!
 
 const numRows = 8
 const numCols = numRows
@@ -21,59 +23,42 @@ const requireClosedTour = false
 
 const unvisited = -1
 
-const timeOutMinutes = 30
+const timeOutMinutes = 60
 
-type offset struct { // legal relative moves from current position.  Positive numbers are down or right.  Must check for legal moves (not off the board or already visited).
+type Offset struct { // legal relative moves from current position.  Positive numbers are down or right.  Must check for legal moves (not off the board or already visited).
 	dr, dc int
 }
 
-var moveOffsets []offset // all possible moves a knight can make.
+var moveOffsets []Offset // all possible moves a knight can make.
 
 var numCalls int64 // number of times the recursive function is called.
 
 var start time.Time // I want this to be global so I can set a timeout of 10 min for now.
 
-func initializeOffsets() {
-	move := offset{
-		dr: 2,
-		dc: 1,
+func initializeOffsets() { // Stephens' code.  Finds a solution in < 8 sec.
+	moveOffsets = []Offset{
+		Offset{-2, -1},
+		Offset{-1, -2},
+		Offset{+2, -1},
+		Offset{+1, -2},
+		Offset{-2, +1},
+		Offset{-1, +2},
+		Offset{+2, +1},
+		Offset{+1, +2},
 	}
-	moveOffsets = append(moveOffsets, move)
-	move = offset{
-		dr: 2,
-		dc: -1,
+}
+
+func initOffsets() { // my code.  Doesn't find a solution in 1 hr.  I'm not setting the timeout for longer.
+	moveOffsets = []Offset{
+		Offset{dr: 2, dc: 1},
+		Offset{dr: 2, dc: -1},
+		Offset{dr: -2, dc: 1},
+		Offset{dr: -2, dc: -1},
+		Offset{dr: 1, dc: 2},
+		Offset{dr: -1, dc: 2},
+		Offset{dr: 1, dc: -2},
+		Offset{dr: -1, dc: -2},
 	}
-	moveOffsets = append(moveOffsets, move)
-	move = offset{
-		dr: -2,
-		dc: 1,
-	}
-	moveOffsets = append(moveOffsets, move)
-	move = offset{
-		dr: -2,
-		dc: -1,
-	}
-	moveOffsets = append(moveOffsets, move)
-	move = offset{
-		dr: 1,
-		dc: 2,
-	}
-	moveOffsets = append(moveOffsets, move)
-	move = offset{
-		dr: -1,
-		dc: 2,
-	}
-	moveOffsets = append(moveOffsets, move)
-	move = offset{
-		dr: 1,
-		dc: -2,
-	}
-	moveOffsets = append(moveOffsets, move)
-	move = offset{
-		dr: -1,
-		dc: -2,
-	}
-	moveOffsets = append(moveOffsets, move)
 }
 
 func makeBoard(rows, cols int) [][]int { // I had to google this to get it right.
@@ -139,7 +124,7 @@ func findTour(board [][]int, numRows, numCols, curRow, curCol, numVisited int) b
 		board[row][col] = numVisited
 		numVisited++
 		if findTour(board, numRows, numCols, row, col, numVisited) { // this recursive call returns true when it succeeds.
-			fmt.Printf(" This move worked: row = %d, col = %d, numVisited = %d\n", row, col, numVisited)
+			//fmt.Printf(" This move worked: row = %d, col = %d, numVisited = %d\n", row, col, numVisited)
 			return true
 		}
 		board[row][col] = unvisited //  undo this proposed move because it didn't work.
@@ -158,7 +143,7 @@ func findTour(board [][]int, numRows, numCols, curRow, curCol, numVisited int) b
 }
 
 func oldmain() {
-	initializeOffsets()
+	//initializeOffsets()
 	board := makeBoard(numRows, numCols)
 	dumpBoard0(board)
 	board[4][4] = 1
@@ -171,6 +156,11 @@ func oldmain() {
 
 }
 
+func init() {
+	//initOffsets()
+	initializeOffsets()
+}
+
 func main() {
 	oldmain()
 	fmt.Printf("\n\n\n")
@@ -178,7 +168,7 @@ func main() {
 	numCalls = 0
 
 	// Initialize the move offsets.
-	initializeOffsets()
+	//initializeOffsets()
 
 	// Create the blank board.
 	board := makeBoard(numRows, numCols)
