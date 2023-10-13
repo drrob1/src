eebfbb3f2ff8e2508670c077ba2a382e709bff8f
Rob Solomon <github@drrob1.com>
2023-10-13 17:53:32 -0400

    On branch master 10/13/2023 17:53:35 liveproject/hash2/hash2.go -- it now works

--- liveproject/hash2/hash2.go
+++ liveproject/hash2/hash2.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"strings"
+	"time"
+)
 
 /*
 This hashtable is based on open addressing, which is also called closed hashing.  This is the 2nd hashing exercise.  Here we use a key's hashed value to directly
@@ -58,16 +63,28 @@ func (hashTable *LinearProbingHashTable) dump() {
 func (hashTable *LinearProbingHashTable) find(name string) (int, int) {
 	// return index of a key's location or the index where it should be inserted, and the probe sequence length.  If not found and table is full, return -1 for the index.
 	// (A + B) mod C = ((A mod C) + (B mod C)) mod C
+	// I found a problem when h is near the end of the table, and those items are not empty.  I want to cycle around before declaring the table full.
+	var jumps int
 	h := hash(name)
 	h = h % hashTable.capacity
 	//fmt.Printf(" In find.  capacity = %d, h = %d\n", hashTable.capacity, h)
-	var i int // keep the i variable for past the for loop
-	for i = h; i < hashTable.capacity; i++ {
+	i := h
+	var loopAround bool
+	for {
+		jumps++
 		if hashTable.employees[i] == nil { // key not found, but came to an empty slot so here's where a new key would be added
-			return i, i - h + 1
+			return i, jumps
 		}
 		if hashTable.employees[i].name == name { // found key
-			return i, i - h + 1
+			return i, jumps
+		}
+		i++
+		if i >= hashTable.capacity {
+			if loopAround { // if already looped around, then the table is full.
+				break
+			}
+			i = 0
+			loopAround = true
 		}
 	}
 	// If got here, name not found and i == capacity
@@ -75,8 +92,8 @@ func (hashTable *LinearProbingHashTable) find(name string) (int, int) {
 }
 
 func (hashTable *LinearProbingHashTable) set(name, phone string) {
-	idx, numJumps := hashTable.find(name)
-	fmt.Printf(" Set %s, %s.  idx = %d, numJumps = %d\n", name, phone, idx, numJumps)
+	idx, _ := hashTable.find(name)
+	//fmt.Printf(" Set %s, %s.  idx = %d, numJumps = %d\n", name, phone, idx, numJumps)
 	if idx < 0 { // hashtable is at capacity, can't add any more entries.
 		panic("hashtable at capacity and cannot add any more entries")
 	}
@@ -147,22 +164,72 @@ func (hashTable *LinearProbingHashTable) dumpConcise() {
 	fmt.Println()
 }
 
-func main() {
+func (hashTable *LinearProbingHashTable) aveProbeSequenceLength() float32 {
+	// Return the average probe sequence length for the items in the table.
+	totalLength := 0
+	numValues := 0
+	for _, employee := range hashTable.employees {
+		if employee != nil {
+			_, probeLength := hashTable.find(employee.name)
+			totalLength += probeLength
+			numValues++
+		}
+	}
+	return float32(totalLength) / float32(numValues)
 }
 
-//type ChainingHashTable struct {
-//	numBuckets int // could be determined using len(), but is easy and helpful here.
-//	buckets    [][]*Employee
-//}
-//func newChainingHashTable(numBuckets int) *ChainingHashTable {
-//	slice := make([][]*Employee, numBuckets)
-//	for i := range slice {
-//		slice[i] = []*Employee{}
-//	}
-//	table := ChainingHashTable{
-//		numBuckets: numBuckets,
-//		buckets:    slice,
-//	}
-//
-//	return &table
-//}
+func main() {
+	// Make some names.
+	employees := []Employee{
+		Employee{"Ann Archer", "202-555-0101"},
+		Employee{"Bob Baker", "202-555-0102"},
+		Employee{"Cindy Cant", "202-555-0103"},
+		Employee{"Dan Deever", "202-555-0104"},
+		Employee{"Edwina Eager", "202-555-0105"},
+		Employee{"Fred Franklin", "202-555-0106"},
+		Employee{"Gina Gable", "202-555-0107"},
+	}
+
+	hashTable := NewLinearProbingHashTable(10)
+	for _, employee := range employees {
+		hashTable.set(employee.name, employee.phone)
+	}
+	hashTable.dump()
+
+	fmt.Printf("Table contains Sally Owens: %t\n", hashTable.contains("Sally Owens"))
+	fmt.Printf("Table contains Dan Deever: %t\n", hashTable.contains("Dan Deever"))
+	// fmt.Println("Deleting Dan Deever")
+	// hashTable.delete("Dan Deever")
+	// fmt.Printf("Table contains Dan Deever: %t\n", hashTable.contains("Dan Deever"))
+	fmt.Printf("Sally Owens: %s\n", hashTable.get("Sally Owens"))
+	fmt.Printf("Fred Franklin: %s\n", hashTable.get("Fred Franklin"))
+	fmt.Println("Changing Fred Franklin")
+	hashTable.set("Fred Franklin", "202-555-0100")
+	fmt.Printf("Fred Franklin: %s\n", hashTable.get("Fred Franklin"))
+
+	// Look at clustering.
+	fmt.Println(time.Now()) // Print the time so it will compile if we use a fixed seed.
+	random := rand.New(rand.NewSource(12345))
+	// random := rand.New(rand.NewSource(time.Now().UnixNano())) // Initialize with a changing seed
+	bigCapacity := 1009
+	bigHashTable := NewLinearProbingHashTable(bigCapacity)
+	numItems := int(float32(bigCapacity) * 0.9)
+	for i := 0; i < numItems; i++ {
+		str := fmt.Sprintf("%d-%d", i, random.Intn(1000000))
+		bigHashTable.set(str, str)
+	}
+	bigHashTable.dumpConcise()
+	fmt.Printf("Average probe sequence length: %f\n",
+		bigHashTable.aveProbeSequenceLength())
+}
+
+func pause() bool {
+	fmt.Print(" Pausing.  Hit <enter> to continue.  Or 'n' to exit  ")
+	var ans string
+	fmt.Scanln(&ans)
+	ans = strings.ToLower(ans)
+	if strings.Contains(ans, "n") {
+		return true
+	}
+	return false
+}
