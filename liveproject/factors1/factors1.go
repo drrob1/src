793c72d50a4faf9732ff9e96169bfbff3726f546
Rob Solomon <github@drrob1.com>
2023-10-20 07:33:48 -0400

    On branch master 10/20/2023 07:33:51 	new file:   liveproject/factors1/factors1.go

---
+++ liveproject/factors1/factors1.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+// For the liveproject.  Starting to write routines to factor numbers.
+
+func findFactors(num int) []int {
+	if num <= 0 {
+		return nil
+	}
+
+	factors := make([]int, 0, 10) // I just plucked 10 out of the air.
+	for num%2 == 0 && num > 0 {   // This loop stops when num is odd.
+		if num%2 == 0 {
+			factors = append(factors, 2)
+			num = num / 2
+		}
+	}
+	if num < 2 { // this means that the number is even.
+		return factors
+	}
+	fmt.Printf(" in findFactors after check for factors of 2:  num=%d, factors so far is/are: %v\n", num, factors)
+
+	// Now all factors of 2 have been handled.  Handle larger factors.  I don't understand the book instructions at all.
+	// Problems so far, the last factor is not getting appended to the slice.  And my sqrt routine doesn't work for small numbers.  I have to see what's a small number by trial and error.
+	factor := 3
+	numSQRT := iSqrt(num)
+	fmt.Printf(" in findFactors before loop: factor=%d, num = %d, numSQRT=%d\n", factor, num, numSQRT)
+	for factor <= numSQRT && num > 1 { // only need to check factors < sqrt(num)
+		if num%factor == 0 {
+			factors = append(factors, factor)
+			num = num / factor
+			continue
+		}
+		// factor is not a factor for num, so increment factor and try again.
+		factor += 2
+		fmt.Printf(" in findFactors loop: factor=%d, num = %d\n", factor, num)
+	}
+
+	return factors
+}
+
+func multiplyFactors(factors []int) int {
+	product := 1
+	for _, fac := range factors {
+		product *= fac
+	}
+	return product
+}
+
+func main() {
+	var num int
+	for {
+		fmt.Printf(" number: ")
+		n, err := fmt.Scan(&num)
+		if num < 2 || err != nil || n == 0 {
+			break
+		}
+
+		start := time.Now()
+
+		factors := findFactors(num)
+		elapsed := time.Since(start)
+		check := multiplyFactors(factors)
+		fmt.Printf(" Number = %d, factors = [%v], in %s, check = %d\n", num, factors, elapsed.String(), check)
+	}
+}
+
+func iSqrt(i int) int { // this uses dividing and averaging
+	if i <= 0 {
+		return 0
+	}
+
+	sqrt := i / 2
+
+	for j := 0; j < 10; j++ {
+		guess := i / sqrt
+		sqrt = (guess + sqrt) / 2
+		if sqrt-guess <= 1 { // recall that this is not floating math.
+			break
+		}
+	}
+
+	return sqrt + 1 // to address an off by 1 problem.
+}
