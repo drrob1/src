a845c8592033ab914032302f8519d59eda07500a
Rob Solomon <github@drrob1.com>
2023-10-21 09:18:53 -0400

    On branch master 10/21/2023 09:18:57 liveproject/factors1/factors1.go -- it works!

--- liveproject/factors1/factors1.go
+++ liveproject/factors1/factors1.go
@@ -12,8 +12,6 @@ func findFactors(num int) []int {
 		return nil
 	}
 
-	number := num
-
 	factors := make([]int, 0, 10) // I just plucked 10 out of the air.
 	for num%2 == 0 && num > 0 {   // This loop stops when num is odd.
 		if num%2 == 0 {
@@ -24,27 +22,29 @@ func findFactors(num int) []int {
 	if num < 2 { // this means that the number is even.
 		return factors
 	}
-	//fmt.Printf(" in findFactors after check for factors of 2:  num=%d, factors so far is/are: %v\n", num, factors)
+	fmt.Printf(" in findFactors after check for factors of 2:  num=%d, factors so far is/are: %v\n", num, factors)
 
 	// Now all factors of 2 have been handled.  Handle larger factors.  I don't understand the book instructions at all.
 	// Problems so far, the last factor is not getting appended to the slice.  And my sqrt routine doesn't work for small numbers.  I have to see what's a small number by trial and error.
+
 	factor := 3
-	//numSQRT := iSqrt(num)  Not working.  I'll take this out for now.
-	//fmt.Printf(" in findFactors before loop: factor=%d, num = %d, numSQRT=gone\n", factor, num)
-	for factor <= num && num > 1 { // only need to check factors < sqrt(num)
+	numSQRT := iSqrt(num)
+	fmt.Printf(" in findFactors before loop: factor=%d, num = %d, numSQRT=%d\n", factor, num, numSQRT)
+	for factor <= numSQRT && num > 1 { // only need to check factors < sqrt(num)
 		if num%factor == 0 {
 			factors = append(factors, factor)
 			num = num / factor
+			fmt.Printf(" in found Factors loop: factor=%d, num = %d\n", factor, num)
 			continue
 		}
 		// factor is not a factor for num, so increment factor and try again.
 		factor += 2
-		//fmt.Printf(" in findFactors loop: factor=%d, num = %d\n", factor, num)
+		fmt.Printf(" in findFactors loop: factor=%d, num = %d\n", factor, num)
 	}
 
 	// If num is prime, return original num as the only factor other than 1, which is not returned.
-	if len(factors) == 0 {
-		factors = append(factors, number)
+	if num > 1 {
+		factors = append(factors, num)
 	}
 
 	return factors
@@ -83,9 +83,9 @@ func iSqrt(i int) int { // this uses dividing and averaging
 		return 0
 	}
 
-	if i < 100 { // the rounding of integer division doesn't work well for small numbers.
-		return i
-	}
+	//if i < 100 { // the rounding of integer division doesn't work well for small numbers.
+	//	return i
+	//}
 
 	sqrt := i / 2
 
