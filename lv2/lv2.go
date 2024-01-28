912044d08d0dc1f2afaef382342780794a85404a
Rob Solomon <github@drrob1.com>
2024-01-28 08:47:14 -0500

    On branch master 01/28/2024 08:47:23 lv2/lv2.go

--- lv2/lv2.go
+++ lv2/lv2.go
@@ -71,7 +71,8 @@ REVISION HISTORY
                 So I deleted it.
 26 Jan 24 -- Still trying to figure out what makes xspf files too large.  So I added output of total file string lengths.  And since my go.mod file says a min of Go 1.21 for compilation,
                 I don't need the init() fcn here to possibly call rand.Seed().  I removed it.  And I'm colorizing the final output message.
-                So far, the maximum string buffer for location strings is 13,937 < max 4 buffer < 13965.
+                So far, the maximum string buffer for location strings is 13,937 < max 4 buffer < 13965.  Nope, that's not it.  See xspftotstrlen.go.
+27 Jan 24 -- Found it.  Bad filenames containing characters that choked vlc.  In this case, !!! was the culprit.  When I removed those files, it worked.  Then I detoxed those files.
 */
 
 /*
