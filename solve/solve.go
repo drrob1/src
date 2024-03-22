64fe018d44dd167e4009d4b2da17b828fb91e955
Rob Solomon <github@drrob1.com>
2024-03-21 20:36:34 -0400

    On branch master 03/21/2024 20:36:39 solve/solve.go

--- solve/solve.go
+++ solve/solve.go
@@ -18,6 +18,9 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	ct "github.com/daviddengcn/go-colortext"
+	ctfmt "github.com/daviddengcn/go-colortext/fmt"
+	gomat "gonum.org/v1/gonum/mat"
 	"io"
 	"os"
 	"runtime"
@@ -30,19 +33,56 @@ import (
 
 const LastCompiled = "21 Feb 24"
 const MaxN = 9
+const small = 1e-10
 
 //                          MaxRealArray is not square because the B column vector is in last column of IM
 
 //type Matrix2D [][]float64  Not used here.  But it is defined in and used by mat.
 
-var verboseFlag = flag.Bool("v", false, "Verbose mode.") // linter says that this is unused.  I'll have to use it, I guess.
+var verboseFlag = flag.Bool("v", false, "Verbose mode.")
+
+func makeDense(matrix mat.Matrix2D) *gomat.Dense {
+	var idx int
+	r := len(matrix)
+	c := len(matrix[0])
+	initDense := make([]float64, r*c)
+	for i := range matrix {
+		for j := range matrix[i] {
+			initDense[idx] = matrix[i][j]
+			idx++
+		}
+	}
+
+	// This may also work.  I have not tested this.
+	//for i := range matrix {
+	//	for j := range matrix[i] {
+	//		Dense.Set(i, j, matrix[i][j])
+	//		idx++
+	//	}
+	//}
+	//
+
+	dense := gomat.NewDense(r, c, initDense)
+	return dense
+}
+
+//func extractDense(m *gomat.Dense) [][]float64 {
+//	r, c := m.Dims()
+//	matrix := mat.NewMatrix(r, c)
+//	for i := range matrix { // different from in mattest2
+//		for j := range matrix[i] { // to see if this works, too.
+//			matrix[i][j] = m.At(i, j)
+//		}
+//	}
+//	return matrix
+//}
 
 func main() {
 	/************************************************************************)
 	  (*                              MAIN PROGRAM                            *)
 	  (************************************************************************/
 
-	fmt.Println(" Equation solver written in Go.  Last compiled ", LastCompiled)
+	fmt.Printf(" Equation solver written in Go.  Last altered %s, compiled with %s\n", LastCompiled, runtime.Version())
 	fmt.Println()
 
 	flag.Usage = func() {
@@ -85,13 +125,15 @@ func main() {
 			i = int(s0 - 'A')
 			filename = filenames[i]
 		}
-		fmt.Println(" Picked filename is", filename)
+		if *verboseFlag {
+			fmt.Println(" Picked filename is", filename)
+		}
 	} else {
 		filename = flag.Arg(0)
-		fmt.Println(" filename on command line is ", filename)
+		if *verboseFlag {
+			fmt.Println(" filename on command line is ", filename)
+		}
 	}
-
-	//  cleancommandline = "xy1.txt";
 
 	infile, err := os.Open(filename)
 	if err != nil {
@@ -142,12 +184,12 @@ CountLinesLoop:
 					IM[lines][col] = token.Rsum // remember that IM is Input Matrix
 					col++
 				} // ENDIF token.state=DGT
-			}            //  UNTIL (EOL is true) OR (col > MaxN);
+			} //  UNTIL (EOL is true) OR (col > MaxN);
 			if col > 0 { // text only or null lines do not increment the row counter.
 				lines++
 			}
 		} // END for n
-	}          // END reading loop
+	} // END reading loop
 	N := lines // Note: lines is 0 origin
 
 	// Now need to create A and B matrices
@@ -168,10 +210,9 @@ CountLinesLoop:
 	mat.Writeln(B, 5)
 	fmt.Println()
 
-	//ans := mat.NewMatrix(N, N)  I don't have to do this.  Obviously, I learned this sometime after 2016 when this code was written.
-	ans := mat.Solve(A, B) // Solve (ra1, ra2, ans, N, 1);
+	X := mat.Solve(A, B)
 	fmt.Println("The solution X to AX = B using Solve is")
-	mat.Writeln(ans, 5)
+	mat.Writeln(X, 5)
 
 	//ans2 := mat.NewMatrix(N, N)
 	ans2 := mat.GaussJ(A, B) // Solve (ra1, ra2, ans, N, 1);
@@ -179,24 +220,106 @@ CountLinesLoop:
 	mat.Writeln(ans2, 5)
 	fmt.Println()
 
-	//  pause();
-
 	// Check that the solution looks right.
 
-	C := mat.Mul(A, ans) // Mul (ra1, ans, N, N, 1, ra3);
-	D := mat.Sub(B, C)   //  Sub (ra3, ra2, N, 1, ra4);
+	C := mat.Mul(A, X) // Mul (ra1, ans, N, N, 1, ra3);
+	D := mat.Sub(B, C) //  Sub (ra3, ra2, N, 1, ra4);
 
 	fmt.Println("As a check, AX-B should be 0, and evaluates to")
 	mat.Writeln(D, 5) //    Write (ra4, N, 1, 4);
 
-	D = mat.BelowSmallMakeZero(D)
+	//D = mat.BelowSmallMakeZero(D)
 
 	fmt.Println("As a check, AX-B should be all zeros after calling BelowSmall.  It evaluates to")
-	mat.Writeln(D, 5)
+	mat.WriteZeroln(D, 5)
 	fmt.Println()
 	fmt.Println()
 
+	newPause()
+
+	// New for the gonum.org code.
+
+	fmt.Printf("---------------------------------------------------------------------------")
+	fmt.Printf(" gonum Test ---------------------------------------------------------------------------\n\n")
+
+	denseA := makeDense(A)
+	denseB := makeDense(B)
+	denseX := makeDense(X) // used below for validation checks.
+	fmt.Printf("A:\n%.5g\n\n", gomat.Formatted(denseA, gomat.Squeeze()))
+	fmt.Printf("B:\n%.5g\n\n", gomat.Formatted(denseB, gomat.Squeeze()))
+
+	// Will try w/ inversion
+	var inverseA, invSoln gomat.Dense
+	err = inverseA.Inverse(denseA)
+	if err != nil {
+		ctfmt.Printf(ct.Red, false, " Error from inverting A: %s.  Bye-Bye\n", err)
+		os.Exit(1)
+	}
+	invSoln.Mul(&inverseA, denseB)
+	fmt.Printf(" Solution by GoNum inversion and B is:\n%.5g\n\n", gomat.Formatted(&invSoln, gomat.Squeeze()))
+
+	// Try LU stuff
+	var lu gomat.LU
+	luSoln := gomat.NewDense(N, 1, nil)
+	lu.Factorize(denseA)
+	err = lu.SolveTo(luSoln, false, denseB)
+	if err != nil {
+		ctfmt.Printf(ct.Red, false, " Error from lu Solve To is %s.  Bye-Bye\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf(" Soluton by gonum LU factorization is:\n%.5g\n\n", gomat.Formatted(luSoln, gomat.Squeeze()))
+
+	// try w/ QR stuff
+	var qr gomat.QR
+	qrSoln := gomat.NewDense(N, 1, nil)
+	qr.Factorize(denseA)
+	err = qr.SolveTo(qrSoln, false, denseB)
+	if err != nil {
+		ctfmt.Printf(ct.Red, false, " Error from qr Solve To is %s.  Bye-Bye\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf(" Soluton by gonum QR factorization is:\n%.5g\n\n", gomat.Formatted(qrSoln, gomat.Squeeze()))
+
+	// Try Solve stuff
+	solvSoln := gomat.NewDense(N, 1, nil) // just to see if this works.
+	err = solvSoln.Solve(denseA, denseB)
+	if err != nil {
+		ctfmt.Printf(ct.Red, false, " Error from Solve is %s.  Bye-bye\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf(" Solution by gonum Solve is:\n%.5g\n\n", gomat.Formatted(solvSoln, gomat.Squeeze()))
+
+	if gomat.EqualApprox(denseX, &invSoln, small) {
+		ctfmt.Printf(ct.Green, false, " X and inversion solution are equal.\n")
+	} else {
+		ctfmt.Printf(ct.Red, true, " X and inversion solution are not equal.\n")
+	}
+	if gomat.EqualApprox(denseX, luSoln, small) {
+		ctfmt.Printf(ct.Green, false, " X and LU solution are equal.\n")
+	} else {
+		ctfmt.Printf(ct.Red, false, " X and LU solution are not equal.\n")
+	}
+	if gomat.EqualApprox(denseX, qrSoln, small) {
+		ctfmt.Printf(ct.Green, false, " X and QR solution are equal.\n")
+	} else {
+		ctfmt.Printf(ct.Red, false, " X and QR solution are not equal.\n")
+	}
+	if gomat.EqualApprox(denseX, solvSoln, small) {
+		ctfmt.Printf(ct.Green, false, " X and Solve solution are equal.\n")
+	} else {
+		ctfmt.Printf(ct.Red, false, " X and Solve solution are not equal.\n")
+	}
+
 } // END Solve.
+
+func newPause() {
+	fmt.Print(" pausing ... hit <enter>  x to stop ")
+	var ans string
+	fmt.Scanln(&ans)
+	if strings.ToLower(ans) == "x" {
+		os.Exit(1)
+	}
+}
 
 //// -------------------------------------------- min ---------------------------------------------
 //func min(a, b int) int {
