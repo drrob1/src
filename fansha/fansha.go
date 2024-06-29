aa4c7501b5f6bd37d9c849fadde402bf2ac834dc
Rob Solomon <github@drrob1.com>
2024-06-29 08:18:03 -0400

    On branch master 06/29/2024 08:18:07 fansha/fansha.go -- undid last commit

--- fansha/fansha.go
+++ fansha/fansha.go
@@ -321,6 +321,6 @@ func main() {
 			runtime.NumGoroutine(), preCounter, postCounter) // counter = 25 is correct.
 	}
 
-	ctfmt.Printf(ct.Yellow, onWin, "\n\n %s: Elapsed time for everything was %s using %d goroutines, precounter = %d.\n\n",
-		os.Args[0], time.Since(t0), runningGoRoutines, preCounter)
+	ctfmt.Printf(ct.Yellow, onWin, "\n\n %s: Elapsed time for everything was %s using %d goroutines = %d.\n\n",
+		os.Args[0], time.Since(t0), runningGoRoutines)
 } // Main for sha.go.
