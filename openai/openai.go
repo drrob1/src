0d5c09466b04f3abb8013365f09e9f8fa1a00bb7
Rob Solomon <github@drrob1.com>
2024-09-01 14:15:55 -0400

    On branch master 09/01/2024 14:15:59 openai/openai.go

--- openai/openai.go
+++ openai/openai.go
@@ -12,7 +12,8 @@ import (
 
   31 Aug 2024 -- Started typing this is from the magazine article.
    1 Sep 2024 -- Separated out the apikey because it seems that github won't allow the key to be committed.
-
+                 I emailed mschilli@perlmeister.com for help.  Admittedly, he wrote this article ~1.5 yrs ago.  The openAI API clearly changed since then.
+                 Mike Schilli
 */
 
 type OpenAI struct {
@@ -31,6 +32,7 @@ func (ai *OpenAI) Init() {
 	}
 	ai.Ctx = context.Background()
 	ai.Cli = gpt3.NewClient(apikey)
+	// ai.Cli = gpt3.NewClient(apikey, gpt3.WithBaseURL("https://api.openai.com/v1/chat/completions"))  Invalid request error
 }
 
 func (ai *OpenAI) PrintResp(prompt string) {
@@ -38,13 +40,19 @@ func (ai *OpenAI) PrintResp(prompt string) {
 		Prompt:      []string{prompt},
 		MaxTokens:   gpt3.IntPtr(1000),
 		Temperature: gpt3.Float32Ptr(0), // allowed values are 0, 1 and 2.  But article says that setting this to 2 makes it hallucinate.
+		Stop:        []string{"."},
+		Echo:        true,
 	}
 	ondata := func(resp *gpt3.CompletionResponse) {
 		fmt.Print(resp.Choices[0].Text)
 	}
 	//turboInstruct := gpt3.GPT3Dot5Turbo + "-instruct"  doesn't work, I'm getting an exceeding free tier message
 	//turboInstruct := gpt3.GPT3Dot5Turbo  doesn't work, I'm getting an exceeding free tier message
-	turboInstruct := "gpt-4o-mini" // doesn't work, giving the same error.
+	//turboInstruct := "gpt-4o-mini" // doesn't work, giving an exceeding free tier message
+	//turboInstruct := "gpt-4o" // doesn't work, giving an exceeding free tier message
+	// err := ai.Cli.CompletionStreamWithEngine(ai.Ctx, "", req, ondata)  Not allow to post on v1/engings/completions
+
+	turboInstruct := gpt3.GPT3Dot5Turbo
 	err := ai.Cli.CompletionStreamWithEngine(ai.Ctx, turboInstruct, req, ondata)
 	if err != nil {
 		panic(err)
