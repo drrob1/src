d0d9ae54720ba8d87ed08f093306ac590c6942b4
Rob Solomon <github@drrob1.com>
2024-12-28 13:08:19 -0500

    On branch master 12/28/2024 13:08:22 list/listutil_windows.go

--- list/listutil_windows.go
+++ list/listutil_windows.go
@@ -77,7 +77,7 @@ func GetFileInfoXFromCommandLine(excludeMe *regexp.Regexp) ([]FileInfoExType, er
 			// Glob returns the names of all files matching pattern or nil if there is no matching file. The syntax of patterns is the same as in Match.
 			// The pattern may describe hierarchical names such as /usr/*/bin/ed (assuming the Separator is '/').  Caveat: it's case sensitive.
 			// Glob ignores file system errors such as I/O errors reading directories. The only possible returned error is ErrBadPattern, when pattern is malformed.
-			fileInfoX, err = FileInfoXFromGlob(dirName)
+			fileInfoX, err = FileInfoXFromGlob(pattern)
 			return fileInfoX, err
 
 			//   removed Dec 28, 2024, as I'm adding more uses of my concurrent code.
