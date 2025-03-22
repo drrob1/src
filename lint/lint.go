fd743802cbf0c76dc3a5a4a578f84abff3912d8e
Rob <git@drrob1.com>
2025-03-21 20:11:52 -0400

    On branch master 03/21/2025 08:11:55 PM lint/lint.go

--- lint/lint.go
+++ lint/lint.go
@@ -535,7 +535,7 @@ func walkRegexFullFilenames() ([]string, error) { // This rtn sorts using sort.S
 	// Put walk func here.  It has to check the directory entry it gets, then search for all filenames that meet the regex and timestamp constraints.
 	walkDirFunction := func(fpath string, de os.DirEntry, err error) error {
 		if *verboseFlag {
-			fmt.Printf(" WalkDir fpath %s, de.name %s, err %v \n", fpath, de.Name(), err.Error())
+			fmt.Printf(" WalkDir fpath %s, de.name invalid, err %v \n", fpath, err.Error())
 		}
 		if err != nil {
 			return filepath.SkipDir
