0e913e7268e4b1cc1ff8d3739cfa23ce11ad33d9
Rob Solomon <github@drrob1.com>
2025-04-13 17:30:25 -0400

    On branch master 04/13/2025 17:30:29 new file:   cpuid/cpuid.go

---
+++ cpuid/cpuid.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	. "github.com/klauspost/cpuid/v2"
+	"strings"
+)
+
+func main() {
+	// Print basic CPU information:
+	fmt.Println("Name:", CPU.BrandName)
+	fmt.Println("PhysicalCores:", CPU.PhysicalCores)
+	fmt.Println("LogicalCores:", CPU.LogicalCores)
+	fmt.Println("ThreadsPerCore:", CPU.ThreadsPerCore)
+
+	fmt.Println("Family", CPU.Family, "Model:", CPU.Model, "Vendor ID:", CPU.VendorID)
+	fmt.Println("Features:", strings.Join(CPU.FeatureSet(), ","))
+
+	fmt.Println("Cacheline bytes:", CPU.CacheLine)
+	fmt.Println("L1 Data Cache:", CPU.Cache.L1D, "bytes")
+	fmt.Println("L1 Instruction Cache:", CPU.Cache.L1I, "bytes")
+	fmt.Println("L2 Cache:", CPU.Cache.L2, "bytes")
+	fmt.Println("L3 Cache:", CPU.Cache.L3, "bytes")
+	fmt.Println("Frequency", CPU.Hz, "hz")
+}
