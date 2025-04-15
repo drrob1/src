62b45820abb4ebf67c8889c15503cd9ef9af864d
Rob Solomon <github@drrob1.com>
2025-04-15 17:17:20 -0400

    On branch master 04/15/2025 17:17:23 cpuid/cpuid.go

--- cpuid/cpuid.go
+++ cpuid/cpuid.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	. "github.com/klauspost/cpuid/v2"
+	"strconv"
 )
 
 /*
 13 Apr 25 -- From chapter 13 of Mastering Go, 4th ed.
+15 Apr 25 -- Added commas to the long numbers
 */
 
 func main() {
@@ -32,5 +34,29 @@ func main() {
 	fmt.Println("L1 Instruction Cache:", CPU.Cache.L1I, "bytes")
 	fmt.Println("L2 Cache:", CPU.Cache.L2, "bytes")
 	fmt.Println("L3 Cache:", CPU.Cache.L3, "bytes")
-	fmt.Println("Frequency", CPU.Hz, "hz")
+	hzStr := strconv.FormatInt(CPU.Hz, 10)
+	hzStr = AddCommas(hzStr)
+	fmt.Printf("Frequency %s hz\n", hzStr)
+}
+
+// InsertIntoByteSlice -- insert a byte into a slice at a designated position.  Intended to insert a comma into a number string.
+func InsertIntoByteSlice(slice, insertion []byte, index int) []byte {
+	return append(slice[:index], append(insertion, slice[index:]...)...)
+}
+
+// AddCommas -- add comas to a number string.
+func AddCommas(instr string) string {
+	Comma := []byte{','}
+
+	//BS := make([]byte, 0, 15)
+	//BS = append(BS, instr...)
+	BS := []byte(instr)
+
+	i := len(BS)
+
+	for NumberOfCommas := i / 3; (NumberOfCommas > 0) && (i > 3); NumberOfCommas-- {
+		i -= 3
+		BS = InsertIntoByteSlice(BS, Comma, i)
+	}
+	return string(BS)
 }
