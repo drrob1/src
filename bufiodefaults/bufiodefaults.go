487ef7eef9a69b9c9a701b35f2f1ee40d3875f73
Rob Solomon <github@drrob1.com>
2025-04-19 13:52:17 -0400

    On branch master 04/19/2025 13:52:22 bufiodefaults/bufiodefaults.go

--- bufiodefaults/bufiodefaults.go
+++ bufiodefaults/bufiodefaults.go
@@ -20,4 +20,30 @@ func main() {
 	fmt.Printf(" reader buffer size from os.Stdin is %d\n", reader.Size())
 	writer := bufio.NewWriter(os.Stdout)
 	fmt.Printf(" writer buffer size from os.Stdout is %d\n", writer.Size())
+	winFile, err := os.Open("MyWin.txt")
+	if err != nil {
+		fmt.Printf("Error opening MyWin.txt: %v\n", err)
+		return
+	}
+	defer winFile.Close()
+	winrdr := bufio.NewReader(winFile)
+	fmt.Printf(" reader buffer size from opening MyWin is %d\n", winrdr.Size())
+
+	f, err := os.CreateTemp(".", "bufiodefaults")
+	if err != nil {
+		fmt.Printf("Error creating TempFile: %v\n", err)
+		return
+	}
+	fmt.Printf(" TempFile created TempFile: %s\n", f.Name())
+	tmpwriter := bufio.NewWriter(f)
+	fmt.Printf(" Writer buffer size from TempFile is %d\n", tmpwriter.Size())
+	tempname := f.Name()
+	f.Close()
+	err = os.Remove(tempname)
+	if err != nil {
+		fmt.Printf("Error removing TempFile: %v\n", err)
+	} else {
+		fmt.Printf(" TempFile removed TempFile: %s\n", tempname)
+	}
+
 }
