87b9fac22283fb057e98c6e7e8a065187f66f2e1
Rob Solomon <github@drrob1.com>
2025-04-26 22:24:56 -0400

    On branch master 04/26/2025 22:24:59 citidb/citidb.go 	new file:   citidb/citidb_test.go

--- citidb/citidb.go
+++ citidb/citidb.go
@@ -11,6 +11,7 @@ import (
 	"os"
 	"regexp"
 	"strconv"
+	"strings"
 	"time"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -18,11 +19,12 @@ import (
 
 /*
    3 Nov 24 -- First version of citidb.go.  This will allow me to insert single records when I'm finished developing it.
-				This is primary intended to enter voided checks into Citibank.db.
+				This is primarily intended to enter voided checks into Citibank.db.
    4 Nov 24 -- Added checkDate, and using a scanner to read in text from the terminal, so spaces are allowed now.
+  26 Apr 25 -- allowing date entry in m/d/y format
 */
 
-const lastAltered = "3 Nov 24"
+const lastAltered = "27 Apr 25"
 
 type transaction struct {
 	status         string
@@ -43,7 +45,7 @@ var (
 // openConnection() function is private and only accessed within the scope of the package
 func openConnection() (*sql.DB, error) {
 	if Filename == "" {
-		return nil, errors.New("Filename is empty")
+		return nil, errors.New("filename is empty")
 	}
 	db, err := sql.Open("sqlite3", Filename) // SQLite3 does not require a username or a password and does not operate over a TCP/IP network.
 	// Therefore, sql.Open() requires just a single parameter, which is the filename of the database.
@@ -55,7 +57,7 @@ func openConnection() (*sql.DB, error) {
 
 func AddRecord(record transaction) error {
 	if record.Date == "" {
-		return errors.New("Date is empty")
+		return errors.New("date is empty")
 	}
 	//if record.Amount == 0 { // amount can, and will likely be, zero.
 	//	return errors.New("Amount is empty")
@@ -101,19 +103,16 @@ func main() {
 		fmt.Printf(" Entered date is in error.  Will use today.\n")
 		date = time.Now().Format(time.DateOnly)
 	}
-	//if !strings.Contains(date, "-") {
-	//	fmt.Printf(" Entered date is in error as it's missing the '-'.  Exiting\n")
-	//	return
-	//}
-	//if len(date) != 10 {
-	//	fmt.Printf(" Entered date is in wrong format as it's length is wrong.  %q was entered.  Exiting\n", date)
-	//	return
-	//}
+
 	if checkDate(date) {
-		fmt.Printf(" Entered date is valid.  Great.\n")
+		fmt.Printf(" Entered date is in YYYY-MM-DD format.\n")
 	} else {
-		fmt.Printf(" Entered date is NOT valid.  Using today.\n")
-		date = time.Now().Format(time.DateOnly)
+		formattedDate := tryDateMDY(date)
+		if formattedDate == "" {
+			fmt.Printf(" Entered date is NOT valid.  Using today.\n")
+			date = time.Now().Format(time.DateOnly)
+		}
+		fmt.Printf(" Entered date is valid.  Using %s.\n", formattedDate)
 	}
 
 	var transactionNumStr string
@@ -181,4 +180,112 @@ func checkDate(date string) bool {
 	regex := regexp.MustCompile(`^\d{4}-\d{2}-\d{2}$`) // staticcheck said to use raw string delimiter so I don't have to escape the backslash.
 	result := regex.MatchString(date)
 	return result
+}
+
+func tryDateMDY(date string) string {
+	regex1 := regexp.MustCompile(`\d{1,2}-\d{1,2}-\d{2}`)
+	result := regex1.MatchString(date)
+	if result { // Input date is in format m-d-yy
+		// Input date is in format m-d-yy.  Will convert to yyyy-mm-dd
+		formattedDate := parseMDYdash(date)
+		return formattedDate // this may be blank if there was an error
+	}
+	regex2 := regexp.MustCompile(`\d{1,2}-\d{1,2}-\d{4}`)
+	result = regex2.MatchString(date)
+	if result {
+		formattedDate := parseMDYdash(date)
+		return formattedDate // this may be blank if there was an error
+	}
+	regex3 := regexp.MustCompile(`\d{1,2}/\d{1,2}/\d{2}`)
+	regex4 := regexp.MustCompile(`\d{1,2}/\d{1,2}/\d{4}`)
+	result = regex3.MatchString(date) || regex4.MatchString(date)
+	if result {
+		formattedDate := parseMDYslash(date)
+		return formattedDate
+	}
+	regex5 := regexp.MustCompile(`\d{1,2}.\d{1,2}.\d{2}`)
+	regex6 := regexp.MustCompile(`\d{1,2}.\d{1,2}.\d{4}`)
+	result = regex5.MatchString(date) || regex6.MatchString(date)
+	if result {
+		formattedDate := parseMDYdot(date)
+		return formattedDate
+	}
+	return ""
+}
+
+func parseMDYdash(date string) string {
+	tokens := strings.Split(date, "-")
+	if len(tokens) != 3 {
+		return ""
+	}
+	month, err := strconv.Atoi(tokens[0])
+	if err != nil {
+		return ""
+	}
+	day, err := strconv.Atoi(tokens[1])
+	if err != nil {
+		return ""
+	}
+	year, err := strconv.Atoi(tokens[2])
+	if err != nil {
+		return ""
+	}
+	if year < 100 {
+		year += 2000
+	}
+	if month < 1 || month > 12 || day < 1 || day > 31 || year < 1900 || year > 2030 {
+		return ""
+	}
+	return fmt.Sprintf("%04d-%02d-%02d", year, month, day)
+}
+
+func parseMDYslash(date string) string {
+	tokens := strings.Split(date, "/")
+	if len(tokens) != 3 {
+		return ""
+	}
+	month, err := strconv.Atoi(tokens[0])
+	if err != nil {
+		return ""
+	}
+	day, err := strconv.Atoi(tokens[1])
+	if err != nil {
+		return ""
+	}
+	year, err := strconv.Atoi(tokens[2])
+	if err != nil {
+		return ""
+	}
+	if year < 100 {
+		year += 2000
+	}
+	if month < 1 || month > 12 || day < 1 || day > 31 || year < 1900 || year > 2030 {
+		return ""
+	}
+	return fmt.Sprintf("%04d-%02d-%02d", year, month, day)
+}
+func parseMDYdot(date string) string {
+	tokens := strings.Split(date, ".")
+	if len(tokens) != 3 {
+		return ""
+	}
+	month, err := strconv.Atoi(tokens[0])
+	if err != nil {
+		return ""
+	}
+	day, err := strconv.Atoi(tokens[1])
+	if err != nil {
+		return ""
+	}
+	year, err := strconv.Atoi(tokens[2])
+	if err != nil {
+		return ""
+	}
+	if year < 100 {
+		year += 2000
+	}
+	if month < 1 || month > 12 || day < 1 || day > 31 || year < 1900 || year > 2030 {
+		return ""
+	}
+	return fmt.Sprintf("%04d-%02d-%02d", year, month, day)
 }
---
+++ citidb/citidb_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func TestTryDateMDY(t *testing.T) {
+	var testString = []struct {
+		input  string
+		output string
+	}{
+		{"3-4-25", "2025-03-04"},
+		{"3-4-2025", "2025-03-04"},
+		{"1/2/25", "2025-01-02"},
+		{"1/2/2025", "2025-01-02"},
+		{"1.2.2025", "2025-01-02"},
+		{"1.2.25", "2025-01-02"},
+		{"10.25.25", "2025-10-25"},
+		{"10.25.2025", "2025-10-25"},
+		{"25.10.2025", ""},
+		{"11;11;25", ""},
+	}
+
+	for _, sampletest := range testString {
+		result := tryDateMDY(sampletest.input)
+		if result != sampletest.output {
+			t.Errorf("tryDateMDY(%s) = %s, want %s", sampletest.input, result, sampletest.output)
+		}
+	}
+}
