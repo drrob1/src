9431cc27f639b09684fa2043bba01e9c02b9890b
Rob Solomon <github@drrob1.com>
2025-07-25 20:19:31 -0400

    On branch master 07/25/2025 20:19:34 new file:   heic/heic.go -- doesn't yet work on Win11

---
+++ heic/heic.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bufio"
+	"github.com/jdeng/goheif"
+	flag "github.com/spf13/pflag"
+	"image/jpeg"
+	"log"
+	"os"
+)
+
+func main() {
+	flag.Parse()
+	fin, fout := flag.Arg(0), flag.Arg(1)
+	fi, err := os.Open(fin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer fi.Close()
+
+	//exif, err := goheif.ExtractExif(fi)
+	//if err != nil {
+	//	fmt.Printf("Warning: no EXIF from %s: %v\n", fin, err)
+	//}
+
+	img, err := goheif.Decode(fi)
+	if err != nil {
+		log.Fatalf("Failed to parse %s: %v\n", fin, err)
+	}
+
+	fo, err := os.OpenFile(fout, os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Fatalf("Failed to create output file %s: %v\n", fout, err)
+	}
+	defer fo.Close()
+
+	w := bufio.NewWriter(fo)
+	defer w.Flush()
+	//if exif != nil {
+	//	_, err = w.Write(exif)
+	//	if err != nil {
+	//		log.Fatalf("Failed to write EXIF to %s: %v\n", fout, err)
+	//	}
+	//	fmt.Printf("Wrote EXIF to %s\n", fout)
+	//}
+	err = jpeg.Encode(w, img, &jpeg.Options{Quality: 100})
+	if err != nil {
+		log.Fatalf("Failed to encode %s: %v\n", fout, err)
+	}
+}
