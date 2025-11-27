b9e74100da3196c6525609cdc6ad2c2500a9edb5
Rob Solomon <github@drrob1.com>
2025-11-26 19:35:12 -0500

    On branch master 11/26/2025 19:35:16 rai/rai.go

--- rai/rai.go
+++ rai/rai.go
@@ -1,9 +1,12 @@
 package main // rai.go
 import (
 	"fmt"
+	"math"
 	"runtime"
 	"strings"
 	"time"
+
+	flag "github.com/spf13/pflag"
 )
 
 /*
@@ -26,22 +29,41 @@ import (
 const lastModified = "26 Nov 25"
 const layout = "01/02/06"
 
+var lambda = math.Log(2) / 8.0249
+
 func main() {
 	fmt.Printf("  rai is last modified %s, compiled with %s \n", lastModified, runtime.Version())
 
-	fmt.Printf(" Enter date for patient to be treated as mm/dd/yy : ")
-	var treatingDateStr string
-	fmt.Scanln(&treatingDateStr)
+	var verboseFlag bool
+	flag.BoolVarP(&verboseFlag, "verbose", "v", false, "Enable verbose output for debugging")
+	flag.Parse()
+
+	var mm, dd, yy int
+
+	fmt.Printf(" Enter date for patient to be treated as mm dd yy : ")
+	_, err := fmt.Scanf("%d %d %d\n", &mm, &dd, &yy)
+	if err != nil {
+		fmt.Printf(" Error scanning for treatingDateStr %s\n", err)
+		fmt.Printf(" Exiting\n")
+		return
+	}
+	treatingDateStr := fmt.Sprintf("%02d/%02d/%02d", mm, dd, yy)
 	treatingDate, err := time.Parse(layout, treatingDateStr)
 	if err != nil {
-		fmt.Printf(" Error from parsing treatingDateStr %s is %s\n", treatingDateStr, err)
+		fmt.Printf(" Error from parsing treatingDateStr %s, using layout %s is %s\n", treatingDateStr, layout, err)
 		fmt.Printf(" Exiting\n")
 		return
 	}
 
-	fmt.Printf(" Enter date of calibration as mm/dd/yy : ")
-	var calibrationDateStr string
-	fmt.Scanln(&calibrationDateStr)
+	fmt.Printf(" Enter date of calibration as mm dd yy : ")
+	_, err = fmt.Scanf("%d %d %d\n", &mm, &dd, &yy)
+	if err != nil {
+		fmt.Printf(" Error from scanning for calibrationDateStr %s\n", err)
+		fmt.Printf(" Exiting\n")
+		return
+	}
+
+	calibrationDateStr := fmt.Sprintf("%02d/%02d/%02d", mm, dd, yy)
 	calibrationDate, err := time.Parse(layout, calibrationDateStr)
 	if err != nil {
 		fmt.Printf(" Error from parsing calibrationDateStr %s is %s\n", calibrationDateStr, err)
@@ -54,14 +76,22 @@ func main() {
 		var switchDatesAns string
 		fmt.Scanln(&switchDatesAns)
 		switchDatesAns = strings.ToLower(switchDatesAns)
-		if strings.Contains(switchDatesAns, "y") {
-			calibrationDate, treatingDate = treatingDate, calibrationDate
+		if strings.Contains(switchDatesAns, "n") { // if not, then exit.
+			return
 		}
+		calibrationDate, treatingDate = treatingDate, calibrationDate
+	}
+
+	deltaT := treatingDate.Sub(calibrationDate).Hours() / 24.0 // calculating it this way makes it a float and not a duration.
+
+	if verboseFlag {
+		fmt.Printf(" Treating Date = %s, Calibration Date = %s\n", treatingDate.Format(layout), calibrationDate.Format(layout))
+		fmt.Printf(" deltaT1 = %g \n", deltaT)
 	}
 
 	fmt.Printf(" Enter desired dose : ")
 	var dose float64
-	fmt.Scanln("%g", &dose)
+	fmt.Scanf("%g\n", &dose)
 	if dose <= 0 {
 		fmt.Printf(" Error: dose must be positive.  Exiting\n")
 		return
@@ -70,4 +100,15 @@ func main() {
 		fmt.Printf(" Error: dose must be less than 33.  Exiting\n")
 		return
 	}
+
+	raiToOrder1 := dose * math.Exp(lambda*deltaT)
+	raiToOrder2 := dose / math.Exp(-lambda*deltaT)
+
+	if verboseFlag {
+		fmt.Printf(" RAI to order 1 = %.2f, RAI to order 2 = %.2f\n", raiToOrder1, raiToOrder2)
+	}
+
+	fmt.Printf(" RAI to order 1 = %.2f, RAI to order 2 = %.2f\n", raiToOrder1, raiToOrder2)
+
+	fmt.Printf(" Done\n\n")
 }
