810b04e365a2052dabc95351fc94f4f62633f83b
Rob Solomon <github@drrob1.com>
2025-12-21 05:43:46 -0500

    On branch master 12/21/2025 05:43:50 markdown/markdown.go 	new file:   markdown/Icon.png

--- markdown/markdown.go
+++ markdown/markdown.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -11,6 +17,7 @@ const width = 800
 const height = 680
 
 func main() {
+	var path, basenameSearchStr string
 	a := app.New()
 	w := a.NewWindow("Markdown Editor")
 
@@ -18,10 +25,88 @@ func main() {
 	previewWidget := widget.NewRichTextFromMarkdown("") // empty string just to initialize it
 	editWidget.OnChanged = previewWidget.ParseMarkdown
 
-	w.SetContent(container.NewAdaptiveGrid(2, editWidget, previewWidget))
+	typedKey := func(ev *fyne.KeyEvent) { // I separated this out so I can more easily understand it.
+		key := string(ev.Name)
+		switch key {
+		case "Q", "Escape", "X":
+			os.Exit(0)
+		}
+	}
+	w.Canvas().SetOnTypedKey(typedKey)
+
+	workingDir, err := os.Getwd()
+	if err != nil {
+		dialog.ShowError(err, w)
+		return
+	}
+	curURI, err := listableFromPath(workingDir)
+	if err != nil {
+		dialog.ShowError(err, w)
+		return
+	}
+
+	fileOpenFunc := func(reader fyne.URIReadCloser, err error) { // this closure gets called AFTER the user has selected a file from the fyne dialog.
+		if err != nil || reader == nil {
+			return
+		}
+		defer reader.Close()
+		path = reader.URI().Path()
+		ext := filepath.Ext(path)
+		basenameSearchStr = filepath.Base(path)
+		basenameSearchStr = strings.TrimSuffix(basenameSearchStr, ext)
+	}
+
+	openBtnFunc := func() { // I want to specify starting directory 1st
+		openDialog := dialog.NewFileOpen(fileOpenFunc, w)
+		openDialog.SetLocation(curURI)
+		openDialog.SetFilter(storage.NewExtensionFileFilter([]string{".md", ".markdown"}))
+		openDialog.Show()
+	}
+	openBtn := widget.NewButton("Open Image", openBtnFunc)
+
+	showSaveFunc := func(win fyne.Window, data []byte) {
+		writeCallback := func(wr fyne.URIWriteCloser, err error) {
+			if err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+			if wr == nil { // user cancelled
+				return
+			}
+			defer wr.Close()
+
+			if _, err := wr.Write(data); err != nil {
+				dialog.ShowError(err, win)
+				return
+			}
+		}
+		wrDialog := dialog.NewFileSave(writeCallback, win)
+		wrDialog.SetLocation(curURI)
+		wrDialog.SetFilter(storage.NewExtensionFileFilter([]string{".md", ".markdown"}))
+		wrDialog.Show()
+	}
+
+	saveBtnFunc := func() {
+		showSaveFunc(w, []byte(editWidget.Text))
+	}
+	saveBtn := widget.NewButton("Save Image", saveBtnFunc)
+
+	quitBtn := widget.NewButton("Quit", func() { os.Exit(0) })
+
+	buttons := container.NewHBox(openBtn, saveBtn, quitBtn)
+	grid := container.NewAdaptiveGrid(2, editWidget, previewWidget)
+	vbox := container.NewVBox(buttons, grid)
+
+	w.SetContent(vbox)
 
 	w.Resize(fyne.NewSize(width, height))
 	w.Canvas().Focus(editWidget)
 	w.ShowAndRun()
 
+}
+
+func listableFromPath(path string) (fyne.ListableURI, error) {
+	u := storage.NewFileURI(path)
+	listerURI, err := storage.ListerForURI(u)
+	return listerURI, err
 }
