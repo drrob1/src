0e7ecb1ec614892d5a2ef0278fad8b842d5e9fa4
Rob Solomon <github@drrob1.com>
2026-02-11 07:55:04 -0500

    On branch master 02/11/2026 07:55:08 timergui/timergui.go

--- timergui/timergui.go
+++ timergui/timergui.go
@@ -7,6 +7,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
 )
 
@@ -15,30 +16,50 @@ import (
 				It can be easily modified to allow user-settable timer duration.
 */
 
+const lastAltered = "11 Feb 26"
+
 func main() {
 
 	a := app.NewWithID("")
 	w := a.NewWindow("Simple Timer")
 	w.Resize(fyne.NewSize(400, 400))
 
-	timerLabel := widget.NewLabel("Time remaining: 2 minutes")
+	durationEntry := widget.NewEntry()
+	durationEntry.SetPlaceHolder(" Enter a duration string")
 
-	go func() {
-		remaining := 120
+	timerLabel := widget.NewLabel("...")
+
+	startTimerFunc := func() {
+		duration, err := time.ParseDuration(durationEntry.Text)
+		if err != nil {
+			dialog.ShowError(err, w)
+		}
+		remaining := int(duration.Seconds())
 		for remaining > 0 {
 			time.Sleep(1 * time.Second)
 			remaining--
+			s1 := fmt.Sprintf("%d", remaining)
+			s2 := fmt.Sprintf("Time remaining: %d seconds", remaining)
 			fyne.Do(func() {
-				timerLabel.SetText(fmt.Sprintf("Time remaining: %d seconds", remaining))
+				w.SetTitle(s1)
+				timerLabel.SetText(s2)
 				// timerLabel.Refresh()  this isn't in the example, and it works.
 			})
 		}
 		fyne.Do(func() {
 			timerLabel.SetText("Time's up")
 		})
-	}()
+	}
 
-	c := container.NewVBox(timerLabel)
+	startTimerBtn := widget.NewButton("Start timer", func() {
+		go startTimerFunc()
+	})
+
+	quitBtn := widget.NewButton("Quit", func() {
+		w.Close()
+	})
+
+	c := container.NewVBox(durationEntry, timerLabel, startTimerBtn, quitBtn)
 	w.SetContent(c)
 	w.ShowAndRun()
 }
