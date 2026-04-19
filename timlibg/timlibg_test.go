17031d64250969420b5eddeb5a82abfa36201e0e
Rob Solomon <rob@drrob1.com>
2026-04-19 00:07:40 -0700

    On branch master 04/19/2026 00:07:44 timlibg/timlibg_test.go -- testing yearnum and daynum

--- timlibg/timlibg_test.go
+++ timlibg/timlibg_test.go
@@ -3,6 +3,7 @@ package timlibg
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func TestJulian(t *testing.T) { // these all pass
@@ -48,5 +49,24 @@ func TestSecToHMS(t *testing.T) { // these tests were mostly written by AI.
 		if Hours != test.Hours || Minutes != test.Minutes || Seconds != test.Seconds {
 			t.Errorf("SecToHMS(%d) = %d:%d:%d, want %d:%d:%d", test.seconds, Hours, Minutes, Seconds, test.Hours, test.Minutes, test.Seconds)
 		}
+	}
+}
+
+func TestGetDateTime(t *testing.T) {
+	now := GetDateTime()
+	juldate := Julian(now.Month, now.Day, now.Year)
+	lastYrJuldate := Julian(12, 31, now.Year-1)
+	myYearDay := juldate - lastYrJuldate
+	if myYearDay != now.YearDayNum {
+		t.Errorf("myYearDay = %d, want %d", myYearDay, now.YearDayNum)
+	}
+	if now.WeekDayNum != juldate%7 {
+		t.Errorf("juldate mod 7 = %d, want %d", juldate%7, now.WeekDayNum)
+	}
+	july4, _ := time.Parse(time.DateOnly, "2026-07-04")
+	july4DayNum := july4.YearDay()
+	JuldateJuly4 := Julian(7, 4, 2026) - lastYrJuldate
+	if july4DayNum != JuldateJuly4 {
+		t.Errorf("july4DanNum = %d, JuldateJuly4 = %d", july4DayNum, JuldateJuly4)
 	}
 }
